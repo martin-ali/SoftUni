797568503d4d9c09a5994bda1591686901241fe3
Tectonik <marto7395@gmail.com>
2018-01-21 01:22:53 +0200

    Finishing up homework

---
+++ JavaScript/simple-calculus/exam-practice.js
@@ -0,0 +1,28 @@
+function classroom([height, width])
+{
+
+}
+
+function vegetableMarket([vegetablePrice, fruitPrice, vegetableWeight, fruitWeight])
+{
+    let totalVegetablePrice = vegetablePrice * vegetableWeight;
+    let totalFruitPrice = fruitPrice * fruitWeight;
+
+    let totalPriceOfAllItemsInEuros = (totalFruitPrice + totalVegetablePrice) / 1.94;
+    return totalPriceOfAllItemsInEuros;
+}
+
+function tilesRepair()
+{
+
+}
+
+function money()
+{
+
+}
+
+function dailyEarnings()
+{
+
+}
