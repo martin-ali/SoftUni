ca80f4297b84789a4dd7cf4e828f1dac93f5228f
Tectonik <marto7395@gmail.com>
2018-01-21 19:52:50 +0200

    Solved "Pool Tubes"

---
+++ CSharp-Fundamentals/Simple-Conditionals/Transport-Price/Pool-Tubes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pool_Tubes
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //var poolCapacity = 1000;
+            //var firstTubeDebit = 100;
+            //var secondTubeDebit = 120;
+            //var howLongWorkerIsMissing = 3d;
+
+            var poolCapacity = double.Parse(Console.ReadLine());
+            var firstTubeDebit = double.Parse(Console.ReadLine());
+            var secondTubeDebit = double.Parse(Console.ReadLine());
+            var howLongWorkerIsMissing = double.Parse(Console.ReadLine());
+
+            double volumeByFirstTube = firstTubeDebit * howLongWorkerIsMissing;
+            double volumeBySecondTube = secondTubeDebit * howLongWorkerIsMissing;
+            double poolCurrentVolume = volumeByFirstTube + volumeBySecondTube;
+
+
+            if (poolCurrentVolume > poolCapacity)
+            {
+                Console.WriteLine($"For {howLongWorkerIsMissing} hours the pool overflows with {poolCurrentVolume - poolCapacity:0.0} liters.");
+            }
+            else
+            {
+                double poolFullness = (poolCurrentVolume / poolCapacity) * 100;
+
+                double x = (volumeByFirstTube / poolCurrentVolume) * 100;
+                double y = (volumeBySecondTube / poolCurrentVolume) * 100;
+
+                Console.WriteLine($"The pool is {Math.Floor(poolFullness)}% full. Pipe 1: {Math.Floor(x)}%. Pipe 2: {Math.Floor(y)}%.");
+            }
+        }
+    }
+}
