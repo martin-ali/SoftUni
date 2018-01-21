595c18b3423a3ae9ab23b231d2b2a9bcda6487af
Tectonik <marto7395@gmail.com>
2018-01-21 16:38:25 +0200

    Finished part I of C# simple conditionals homework

--- CSharp-Fundamentals/Simple-Conditionals/Big-Number-to-Text/Program.cs
+++ CSharp-Fundamentals/Simple-Conditionals/Big-Number-to-Text/Program.cs
@@ -1,36 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Big_Number_to_Text
 {
     class Program
     {
+        private static Dictionary<int, string> numberToTextMap = new Dictionary<int, string>()
+        {
+            [0] = "zero",
+            [1] = "one",
+            [2] = "two",
+            [3] = "three",
+            [4] = "four",
+            [5] = "five",
+            [6] = "six",
+            [7] = "seven",
+            [8] = "eight",
+            [9] = "nine",
+            [10] = "ten",
+            [11] = "eleven",
+            [12] = "twelve",
+            [13] = "thirteen",
+            [14] = "fourteen",
+            [15] = "fifteen",
+            [16] = "sixteen",
+            [17] = "seventeen",
+            [18] = "eighteen",
+            [19] = "nineteen",
+            [20] = "twenty",
+            [30] = "thirty",
+            [40] = "forty",
+            [50] = "fifty",
+            [60] = "sixty",
+            [70] = "seventy",
+            [80] = "eighty",
+            [90] = "ninety",
+            [100] = "one hundred"
+        };
+
         static void Main()
         {
             var number = int.Parse(Console.ReadLine());
 
-            if (number == 0)
+            if (number == 100 || (number >= 0 && number <= 19) || (number % 10) == 0)
             {
-
-            }
-            else if (number > 0 && number <= 9)
-            {
-
-            }
-            else if (number >= 10 && number <= 19)
-            {
-
+                var result = numberToTextMap[number];
+                Console.WriteLine(result);
             }
             else if (number >= 20 && number <= 99)
             {
+                int wholePart = number - (number % 10);
+                int smallPart = number - wholePart;
 
+                string wholePartText = numberToTextMap[wholePart];
+                string smallPartText = numberToTextMap[smallPart];
+
+                Console.WriteLine($"{wholePartText} {smallPartText}");
             }
-            else if (number == 100)
+            else
             {
-
+                Console.WriteLine("invalid number");
             }
         }
     }
