e852b58eaf50bc1847870b02c6223fe3ccb9f69e
Tectonik <marto7395@gmail.com>
2018-01-21 20:16:02 +0200

    Solved "Sleepycat Tom"

---
+++ CSharp-Fundamentals/Simple-Conditionals/Transport-Price/Sleepycat-Tom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sleepycat_Tom
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfOffDays = int.Parse(Console.ReadLine());
+            int numberOfWorkdays = 365 - numberOfOffDays;
+
+            int hoursOfPlay = (numberOfOffDays * 127) + (numberOfWorkdays * 63);
+            int normalPlaytime = 30000;
+
+            int deltaFromNorm = Math.Abs(hoursOfPlay - normalPlaytime);
+            int hoursDelta = deltaFromNorm / 60;
+            int minutesDelta = deltaFromNorm - (hoursDelta * 60);
+
+            if (hoursOfPlay > normalPlaytime)
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{hoursDelta} hours and {minutesDelta} minutes more for play");
+            }
+            else if (hoursOfPlay < normalPlaytime)
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{hoursDelta} hours and {minutesDelta} minutes less for play");
+            }
+        }
+    }
+}
