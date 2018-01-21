e1c9f1d9033a60f2333fa047b884024242f43cb7
Tectonik <marto7395@gmail.com>
2018-01-21 17:35:44 +0200

    Added new homework solution, solved "Transport Price"

---
+++ CSharp-Fundamentals/Simple-Conditionals/Transport-Price/Simple-Conditionals-II/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Simple_Conditionals_II
+{
+    class Program
+    {
+        private static decimal taxiStartTax = 0.70m;
+        private static decimal taxiDayPrice = 0.79m;
+        private static decimal taxiNightPrice = 0.90m;
+
+        private static int bisMinTravelLength = 20;
+        private static decimal busBricing = 0.09m;
+
+        private static int trainMinTravelLength = 100;
+        private static decimal trainPricing = 0.06m;
+
+        static void Main()
+        {
+
+            var travelLength = int.Parse(Console.ReadLine());
+            var timeOfDay = Console.ReadLine();
+
+            decimal priceOfTaxiFare = decimal.MaxValue;
+            decimal priceOfBusFare = decimal.MaxValue;
+            decimal priceOfTrainFare = decimal.MaxValue;
+
+            decimal taxiPriceForTravel = travelLength * (timeOfDay == "day" ? taxiDayPrice : taxiNightPrice);
+            priceOfTaxiFare = taxiStartTax + taxiPriceForTravel;
+
+            if (travelLength >= 20)
+            {
+                priceOfBusFare = travelLength * busBricing;
+            }
+
+            if (travelLength >= 100)
+            {
+                priceOfTrainFare = travelLength * trainPricing;
+            }
+
+            decimal result = Math.Min(Math.Min(priceOfTaxiFare, priceOfBusFare), priceOfTrainFare);
+            Console.WriteLine($"{result:0.00}");
+        }
+    }
+}
