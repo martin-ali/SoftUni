3c17b427e26dfb81f0abe6839b1c53989f240374
Tectonik <marto7395@gmail.com>
2018-01-31 00:02:02 +0200

    Solved "Cinema"; Added project for "Voleyball"

---
+++ Csharp-Fundamentals/Complex-Conditionals/Complex-Conditionals-Homework/Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main()
+        {
+            var projectionType = Console.ReadLine().ToLower();
+            var rowCount = int.Parse(Console.ReadLine());
+            var colCount = int.Parse(Console.ReadLine());
+
+            var rate = 0d;
+
+            if (projectionType == "premiere")
+            {
+                rate = 12;
+            }
+            else if (projectionType == "normal")
+            {
+                rate = 7.5;
+            }
+            else if (projectionType == "discount")
+            {
+                rate = 5;
+            }
+
+            var result = rate * rowCount * colCount;
+            Console.WriteLine($"{result:0.00} leva");
+        }
+    }
+}
---
+++ Csharp-Fundamentals/Complex-Conditionals/Complex-Conditionals-Homework/Voleyball/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voleyball
+{
+    class Program
+    {
+        static void Main()
+        {
+            var yearType = Console.ReadLine().ToLower();
+            var rowCount = int.Parse(Console.ReadLine());
+            var colCount = int.Parse(Console.ReadLine());
+
+            
+
+            var result = 0;
+            Console.WriteLine($"{result:0.00}");
+        }
+    }
+}
