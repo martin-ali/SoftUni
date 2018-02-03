72d25b0f0ca8a59156c4c5cbee868d2d7f9a7ac1
Tectonik <marto7395@gmail.com>
2018-02-03 20:56:09 +0200

    Solved "Max number"

---
+++ Csharp-Fundamentals/Loops/Loops/Loops/Max-Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numberOfInputs = int.Parse(Console.ReadLine());
+            var biggestNumber = int.MinValue;
+
+            for (int current = 0; current < numberOfInputs; current++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber > biggestNumber)
+                {
+                    biggestNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(biggestNumber);
+        }
+    }
+}
