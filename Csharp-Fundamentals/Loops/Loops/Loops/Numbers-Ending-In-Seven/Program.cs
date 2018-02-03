f907dd18019d0cd27de7e9be0bf7d27c27d63cb2
Tectonik <marto7395@gmail.com>
2018-02-03 20:29:35 +0200

    Solved "Numbers ending in 7"

---
+++ Csharp-Fundamentals/Loops/Loops/Loops/Numbers-Ending-In-Seven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Numbers_Ending_In_Seven
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int current = 1; current <= 1000; current++)
+            {
+                bool numberEndsInSeven = current % 10 == 7;
+                if (numberEndsInSeven)
+                {
+                    Console.WriteLine(current);
+                }
+            }
+        }
+    }
+}
