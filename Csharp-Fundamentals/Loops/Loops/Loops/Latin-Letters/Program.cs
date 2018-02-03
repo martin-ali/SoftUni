1286172b74e998b6377afff39df4c93f3986e076
Tectonik <marto7395@gmail.com>
2018-02-03 20:29:46 +0200

    Solved "Latin letters"

---
+++ Csharp-Fundamentals/Loops/Loops/Loops/Latin-Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Latin_Letters
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int current = 'a'; current <= 'z'; current++)
+            {
+                Console.WriteLine((char)current);
+            }
+        }
+    }
+}
