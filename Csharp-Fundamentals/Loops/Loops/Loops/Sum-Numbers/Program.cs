8145a49fb7ba1fec12db66347bf531b0b980fc7c
Tectonik <marto7395@gmail.com>
2018-02-03 20:53:37 +0200

    Solved "Sum numbers"

---
+++ Csharp-Fundamentals/Loops/Loops/Loops/Sum-Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numberOfInputs = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int current = 0; current < numberOfInputs; current++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+                sum += currentNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
