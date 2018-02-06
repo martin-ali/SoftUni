b38059c55c23c7b76b6b30c96749faec188f679d
Tectonik <marto7395@gmail.com>
2018-02-06 21:58:24 +0200

    Started complex conditionals homework Part II

---
+++ Csharp-Fundamentals/Complex-Conditionals/Complex-Conditionals-Exam-Homework/On-Time-For-The-Exam/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace On_Time_For_The_Exam
+{
+    class Program
+    {
+        static void Main()
+        {
+            var examHours = int.Parse(Console.ReadLine());
+            var examMinutes = int.Parse(Console.ReadLine());
+            var studentHours = int.Parse(Console.ReadLine());
+            var studentMinutes = int.Parse(Console.ReadLine());
+
+            var result = 0;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Csharp-Fundamentals/Complex-Conditionals/Complex-Conditionals-Exam-Homework/On-Time-For-The-Exam/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("On-Time-For-The-Exam")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("On-Time-For-The-Exam")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("56ecd979-4562-47f2-837e-c4d10b31215d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
