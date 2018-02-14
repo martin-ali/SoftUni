1e93382907783fbf150cc898bfcb9287af7a63c1
Tectonik <marto7395@gmail.com>
2018-02-14 17:01:21 +0200

    Moved loops homework project folder

--- Csharp-Fundamentals/Complex-Loops/Complex-Loops/Numbers-From-1-To-100/Program.cs
+++ Csharp-Fundamentals/Complex-Loops/Complex-Loops/Numbers-From-1-To-100/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Numbers_From_1_To_100
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            for (int current = 1; current <= 100; current++)
+            {
+                Console.WriteLine(current);
+            }
         }
     }
 }
