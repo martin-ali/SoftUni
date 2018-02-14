85de132ca71b1d56c9646f44c105a5688e3e4974
Tectonik <marto7395@gmail.com>
2018-02-14 13:11:56 +0200

    Initialised "Drawing figures with loops" homework

---
+++ Csharp-Fundamentals/Drawing-Figures/Drawing-Figures-With-Loops/Rectangle-Of-Stars-10x10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Of_Stars_10x10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
