a1d44f4074da34a1645fc2c2befc22365d3a4814
Tectonik <marto7395@gmail.com>
2018-02-14 16:53:34 +0200

    Final edit to "Complex Conditionals" homework

--- Csharp-Fundamentals/Complex-Conditionals/Complex-Conditionals-Homework/Voleyball/Program.cs
+++ Csharp-Fundamentals/Complex-Conditionals/Complex-Conditionals-Homework/Voleyball/Program.cs
@@ -10,11 +10,10 @@ namespace Voleyball
     {
         static void Main()
         {
+            // Could not understand problem description, thus I have abandoned this problem 
             var yearType = Console.ReadLine().ToLower();
             var numberOfHolidaysInYear = int.Parse(Console.ReadLine());
             var numberOfWeekends = int.Parse(Console.ReadLine());
-
-
 
             var result = 0;
             Console.WriteLine($"{result:0.00}");
