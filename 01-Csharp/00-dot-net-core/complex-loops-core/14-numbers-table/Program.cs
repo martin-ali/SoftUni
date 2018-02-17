91528e94d3b441cdbc4e2420ba92bc5e85fbc397
Tectonik <marto7395@gmail.com>
2018-02-18 00:09:47 +0200

    Working on "Complex loops" homework in .Net Core
    Using Fedora, Vscode, GitKraken

---
+++ 01-Csharp/00-dot-net-core/complex-loops-core/14-numbers-table/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _14_numbers_table
+{
+    class Program
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+
+            for (int row = 0; row < size; row++)
+            {
+                var currentRow = new int[size];
+                for (int col = 0, decider = 0; col < size; col++)
+                {
+                    var x=0;
+                    var y=true;
+
+                    if (currentNumber + col <= size)
+                    {
+                        y=false;
+                    }
+
+                    if(y)
+                    {
+                        x = currentNumber+decider;
+                        decider++;
+                    }
+                    else
+                    {
+                        x = currentNumber+decider;
+                        decider--;
+                    }    
+                    
+                    currentRow[col] = x;
+                }
+
+                currentNumber++;
+                Console.WriteLine(String.Join(" ", currentRow));
+            }
+        }
+    }
+}
