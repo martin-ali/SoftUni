a797cbc81c96d16977aa2ce283fba750bdf2d3d8
Tectonik <marto7395@gmail.com>
2018-02-21 20:47:11 +0200

    Finished complex loops homework

--- 01-Csharp/00-Fundamentals/07-Complex-Loops/complex-loops-core/07-greatest-common-divisor/Program.cs
+++ 01-Csharp/00-Fundamentals/07-Complex-Loops/complex-loops-core/07-greatest-common-divisor/Program.cs
@@ -6,8 +6,34 @@ namespace _07_greatest_common_divisor
     {
         static void Main()
         {
-            var numA = int.Parse(Console.ReadLine());
-            var numB = int.Parse(Console.ReadLine());
+            int numA = int.Parse(Console.ReadLine());
+            int numB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(findGreatestCommonDivisor(numA, numB));
+        }
+
+        static int findGreatestCommonDivisor(int a, int b)
+        {
+            // Find more elegant way
+            if (a == 0)
+            {
+                return b;
+            }
+            if (b == 0)
+            {
+                return a;
+            }
+
+            if (a > b)
+            {
+                int greatestCommonDivisor = findGreatestCommonDivisor(a % b, b);
+                return greatestCommonDivisor;                
+            }
+            else
+            {
+                int greatestCommonDivisor = findGreatestCommonDivisor(a, b % a);
+                return greatestCommonDivisor; 
+            }
         }
     }
 }
