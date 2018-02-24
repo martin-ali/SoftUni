160d6d48afd34ecf8009940401ced7b2b5138873
Tectonik <marto7395@gmail.com>
2018-02-24 13:54:29 +0200

    Starting "On time for the exam"

---
+++ 01-Csharp/00-Fundamentals/04-Complex-Conditionals/Exam-Homework/on-time-for-the-exam/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace on_time_for_the_exam
+{
+    class Program
+    {
+        static void Main()
+        {
+            int examHours = int.Parse(Console.ReadLine());
+            int examMinutes = int.Parse(Console.ReadLine());
+
+            int arrivalHours = int.Parse(Console.ReadLine());
+            int arrivalMinutes = int.Parse(Console.ReadLine());
+            
+        }
+    }
+}
