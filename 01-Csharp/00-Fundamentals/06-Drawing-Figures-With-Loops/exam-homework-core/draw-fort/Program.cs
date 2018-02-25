688cfb9e5d9ebf01a3d1f03a84e98e13b4561d03
Tectonik <marto7395@gmail.com>
2018-02-26 00:52:55 +0200

    Solved "Draw fort"

--- 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-fort/Program.cs
+++ 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-fort/Program.cs
@@ -7,7 +7,34 @@ namespace draw_fort
         static void Main()
         {
             int size = int.Parse(Console.ReadLine());
-            
+
+            // Draw top
+            var widthBetweenTowers = (2 * size) - 4 - ((size / 2) * 2);
+            string towerTop = new String('^', size / 2);
+            string topMiddleSection = new String('_', widthBetweenTowers);
+            Console.WriteLine($"/{towerTop}\\{topMiddleSection}/{towerTop}\\");
+
+            // Draw midsection
+            for (int row = 2; row < size; row++)
+            {
+                if (row == size - 1)
+                {
+                    var emptySpaceWidth = ((size * 2) - 2 - widthBetweenTowers) / 2;
+                    string wall = new String(' ', emptySpaceWidth);
+                    string bottomSectionBetweenTowers = new String('_', widthBetweenTowers);
+                    Console.WriteLine($"|{wall}{bottomSectionBetweenTowers}{wall}|");
+                }
+                else
+                {
+                    string wall = new String(' ', (size * 2) - 2);
+                    Console.WriteLine($"|{wall}|");
+                }
+            }
+
+            // Draw bottom
+            string towerBottom = new String('_', size / 2);
+            string bottomMiddleSection = new String(' ', widthBetweenTowers);
+            Console.WriteLine($"\\{towerBottom}/{bottomMiddleSection}\\{towerBottom}/");
         }
     }
 }
