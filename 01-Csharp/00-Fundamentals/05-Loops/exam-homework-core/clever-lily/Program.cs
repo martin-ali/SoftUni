cc94e7c1e7cc16fd4e72dbf5663e20e0629a3b5b
Tectonik <marto7395@gmail.com>
2018-02-25 12:01:12 +0200

    Finished "Clever Lily"

--- 01-Csharp/00-Fundamentals/05-Loops/exam-homework-core/clever-lily/Program.cs
+++ 01-Csharp/00-Fundamentals/05-Loops/exam-homework-core/clever-lily/Program.cs
@@ -4,9 +4,38 @@ namespace clever_lily
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int ageOfLily = int.Parse(Console.ReadLine());
+            decimal washingMachinePrice = decimal.Parse(Console.ReadLine());
+            int pricePerToy = int.Parse(Console.ReadLine());
+            
+            int numberOfToysOwned = 0;
+            decimal sumOfMoneyOwned = 0;
+            for (int birthday = 1, giftMoney = 10; birthday <= ageOfLily; birthday++)
+            {
+                if (birthday % 2 == 0)
+                {
+                    sumOfMoneyOwned -= 1;
+                    sumOfMoneyOwned += giftMoney;
+                    giftMoney += 10;
+                }
+                else
+                {
+                    numberOfToysOwned++;
+                }
+            }
+
+            var totalSumGathered = sumOfMoneyOwned + (numberOfToysOwned * pricePerToy);
+            
+            if (totalSumGathered >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {totalSumGathered - washingMachinePrice:0.00}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingMachinePrice - totalSumGathered:0.00}");                
+            }
         }
     }
 }
