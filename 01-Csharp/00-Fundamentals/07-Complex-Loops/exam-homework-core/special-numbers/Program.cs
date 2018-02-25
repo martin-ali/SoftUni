f52f7c0b4db055f65215504f5aff1bd83a60be42
Tectonik <marto7395@gmail.com>
2018-02-25 18:29:36 +0200

    Finished "Special numbers" without .Net Core; .Net Core now broken

--- 01-Csharp/00-Fundamentals/07-Complex-Loops/exam-homework-core/special-numbers/Program.cs
+++ 01-Csharp/00-Fundamentals/07-Complex-Loops/exam-homework-core/special-numbers/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
 
 namespace special_numbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int specialNumber = int.Parse(Console.ReadLine());
+
+            for (int first = 1; first <= 9; first++)
+            {
+                for (int second = 1; second <= 9; second++)
+                {
+                    for (int third = 1; third <= 9; third++)
+                    {
+                        for (int fourth = 1; fourth <= 9; fourth++)
+                        {
+                            var magicNumberIsDivisibleByAllDigits = specialNumber % first == 0
+                                    && specialNumber % second == 0
+                                    && specialNumber % third == 0
+                                    && specialNumber % fourth == 0;
+                                    
+                            if (magicNumberIsDivisibleByAllDigits)
+                            {
+                                Console.Write($"{first}{second}{third}{fourth} ");
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
