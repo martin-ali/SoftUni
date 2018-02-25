8f278188f521cbfd9fb74e75b1864d9bf495f107
Tectonik <marto7395@gmail.com>
2018-02-25 11:39:03 +0200

    Started "Clever lily"

---
+++ 01-Csharp/00-Fundamentals/05-Loops/exam-homework-core/clever-lily/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clever_lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
