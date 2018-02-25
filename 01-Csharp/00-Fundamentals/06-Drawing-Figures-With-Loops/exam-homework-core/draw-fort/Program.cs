4b630715704cc72b8e603140f2e95c422bf210cb
Tectonik <marto7395@gmail.com>
2018-02-25 14:35:23 +0200

    Re-organisation

---
+++ 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-fort/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace draw_fort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            
+        }
+    }
+}
---
+++ 01-Csharp/00-Fundamentals/07-Complex-Loops/exam-homework-core/dumb-password-generator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dumb_password_generator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int firstNumber = 1; firstNumber < n; firstNumber++)
+            {
+                for (int secondNumber = 1; secondNumber < n; secondNumber++)
+                {
+                    for (int firstLetter = 'a'; firstLetter < n + 'a'; firstLetter++)
+                    {
+                        for (int secondLetter = 'a'; secondLetter < n + 'a'; secondLetter++)
+                        {
+                            for (int thirdNumber = Math.Max(firstNumber, secondNumber); thirdNumber < n; thirdNumber++)
+                            {
+                                Console.Write($"{firstNumber}{secondNumber}{firstLetter}{secondLetter}{thirdNumber} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
