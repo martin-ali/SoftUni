c7c936d3a6d1c94e6457f75f5ad8a93752284926
Tectonik <marto7395@gmail.com>
2018-02-25 14:40:47 +0200

    Finished "Stupid password generator"

--- 01-Csharp/00-Fundamentals/07-Complex-Loops/exam-homework-core/dumb-password-generator/Program.cs
+++ 01-Csharp/00-Fundamentals/07-Complex-Loops/exam-homework-core/dumb-password-generator/Program.cs
@@ -9,17 +9,17 @@ namespace dumb_password_generator
             int n = int.Parse(Console.ReadLine());
             int l = int.Parse(Console.ReadLine());
 
-            for (int firstNumber = 1; firstNumber < n; firstNumber++)
+            for (int firstNumber = 1; firstNumber <= n; firstNumber++)
             {
-                for (int secondNumber = 1; secondNumber < n; secondNumber++)
+                for (int secondNumber = 1; secondNumber <= n; secondNumber++)
                 {
-                    for (int firstLetter = 'a'; firstLetter < n + 'a'; firstLetter++)
+                    for (int firstLetter = 'a'; firstLetter < 'a' + l; firstLetter++)
                     {
-                        for (int secondLetter = 'a'; secondLetter < n + 'a'; secondLetter++)
+                        for (int secondLetter = 'a'; secondLetter < 'a' + l; secondLetter++)
                         {
-                            for (int thirdNumber = Math.Max(firstNumber, secondNumber); thirdNumber < n; thirdNumber++)
+                            for (int thirdNumber = Math.Max(firstNumber, secondNumber) + 1; thirdNumber <= n; thirdNumber++)
                             {
-                                Console.Write($"{firstNumber}{secondNumber}{firstLetter}{secondLetter}{thirdNumber} ");
+                                Console.Write($"{firstNumber}{secondNumber}{(char)firstLetter}{(char)secondLetter}{thirdNumber} ");
                             }
                         }
                     }
