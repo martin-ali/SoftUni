f67114a2e7311523c0978c78835686f424f79a5d
Tectonik <marto7395@gmail.com>
2018-02-26 14:15:47 +0200

    Finished "Draw stop sign"

--- 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-stop-sign/Program.cs
+++ 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-stop-sign/Program.cs
@@ -4,9 +4,43 @@ namespace draw_stop_sign
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            int firstSectionHeight = size + 1;
+            int signHeight = (firstSectionHeight * 2) + 1;
+            int signWidth = (size + 1) + ((size * 2) + 1) + (size + 1);
+
+            // Draw top
+            int dotSectionWidth = size + 1;
+            int underscoreSectionWidth = (size * 2) + 1;
+            for (int row = 0; row < firstSectionHeight; row++, dotSectionWidth--, underscoreSectionWidth += 2)
+            {
+                var dots = new String('.', dotSectionWidth);
+                if (row > 0)
+                {
+                    var underscores = new String('_', underscoreSectionWidth - 4);
+                    Console.WriteLine($"{dots}//{underscores}\\\\{dots}");
+                }
+                else
+                {
+                    var underscores = new String('_', underscoreSectionWidth);
+                    Console.WriteLine($"{dots}{underscores}{dots}");
+                }
+            }
+
+            // Draw mid
+            // 4 diagonal lines, 5 letters in "STOP!"
+            var side = new String('_', (signWidth - 4 - 5) / 2);
+            Console.WriteLine($"//{side}STOP!{side}\\\\");
+
+            // Draw bottom
+            for (int row = size; row > 0; row--, dotSectionWidth++, underscoreSectionWidth -= 2)
+            {
+                var dots = new String('.', dotSectionWidth);
+                var undescores = new String('_', underscoreSectionWidth - 4);
+                Console.WriteLine($"{dots}\\\\{undescores}//{dots}");
+            }
         }
     }
 }
