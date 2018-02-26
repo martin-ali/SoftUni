03c6b25e4fbb45ed230377ebd514fb2a0e2436e8
Tectonik <marto7395@gmail.com>
2018-02-26 11:34:21 +0200

    Finished "Draw butterfly"

--- 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-butterfly/Program.cs
+++ 01-Csharp/00-Fundamentals/06-Drawing-Figures-With-Loops/exam-homework-core/draw-butterfly/Program.cs
@@ -4,9 +4,46 @@ namespace draw_butterfly
 {
     class Program
     {
-        static void Main(string[] args)
+        // Could be written elegantly with a single loop, this method chosen for simplicity
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int size = int.Parse(Console.ReadLine());
+            int height = 2 * (size - 2) + 1;
+            int width = (2 * size) - 1;
+
+            // Draw top
+            int topHeight = size - 2;
+            string wingStar = new String('*', size - 2);
+            string wingDash = new String('-', size - 2);
+            for (int row = 2; row < size; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    Console.WriteLine($"{wingStar}\\ /{wingStar}");
+                }
+                else
+                {
+                    Console.WriteLine($"{wingDash}\\ /{wingDash}");
+                }
+            }
+
+            // Draw mid
+            int midHeight = 1;
+            Console.WriteLine($"{new String(' ', size - 1)}@");
+
+            // Draw bottom
+            int bottomHeight = size - 2;
+            for (int row = 2; row < size; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    Console.WriteLine($"{wingStar}/ \\{wingStar}");
+                }
+                else
+                {
+                    Console.WriteLine($"{wingDash}/ \\{wingDash}");
+                }
+            }
         }
     }
 }
