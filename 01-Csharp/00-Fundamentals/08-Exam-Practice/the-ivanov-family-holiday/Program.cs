efcabcdb2560235c9e1920fcf8e0bd2c673adeeb
Tectonik <marto7395@gmail.com>
2018-03-03 09:46:51 +0200

    Changed strings for age to enum

--- 01-Csharp/00-Fundamentals/08-Exam-Practice/the-ivanov-family-holiday/Program.cs
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/the-ivanov-family-holiday/Program.cs
@@ -9,82 +9,84 @@ namespace the_ivanov_family_holiday
 
         enum StayLength { Short, Medium, Long };
 
+        enum Age { Adult, Child};
+
         static void Main()
         {
-            var destinationMap = new Dictionary<string, Dictionary<StayLength, Dictionary<string, decimal>>>()
+            var destinationMap = new Dictionary<string, Dictionary<StayLength, Dictionary<Age, decimal>>>()
             {
-                ["miami"] = new Dictionary<StayLength, Dictionary<string, decimal>>()
+                ["miami"] = new Dictionary<StayLength, Dictionary<Age, decimal>>()
                 {
-                    [StayLength.Short] = new Dictionary<string, decimal>()
+                    [StayLength.Short] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 24.99m,
-                        ["child"] = 14.99m
+                        [Age.Adult] = 24.99m,
+                        [Age.Child] = 14.99m
                     },
-                    [StayLength.Medium] = new Dictionary<string, decimal>()
+                    [StayLength.Medium] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 22.99m,
-                        ["child"] = 11.99m
+                        [Age.Adult] = 22.99m,
+                        [Age.Child] = 11.99m
                     },
-                    [StayLength.Long] = new Dictionary<string, decimal>()
+                    [StayLength.Long] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 20m,
-                        ["child"] = 10m
+                        [Age.Adult] = 20m,
+                        [Age.Child] = 10m
                     }
                 },
-                ["canary islands"] = new Dictionary<StayLength, Dictionary<string, decimal>>()
+                ["canary islands"] = new Dictionary<StayLength, Dictionary<Age, decimal>>()
                 {
-                    [StayLength.Short] = new Dictionary<string, decimal>()
+                    [StayLength.Short] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 32.50m,
-                        ["child"] = 28.50m
+                        [Age.Adult] = 32.50m,
+                        [Age.Child] = 28.50m
                     },
-                    [StayLength.Medium] = new Dictionary<string, decimal>()
+                    [StayLength.Medium] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 30.50m,
-                        ["child"] = 25.60m
+                        [Age.Adult] = 30.50m,
+                        [Age.Child] = 25.60m
                     },
-                    [StayLength.Long] = new Dictionary<string, decimal>()
+                    [StayLength.Long] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 28m,
-                        ["child"] = 22m
+                        [Age.Adult] = 28m,
+                        [Age.Child] = 22m
                     }
                 },
-                ["philippines"] = new Dictionary<StayLength, Dictionary<string, decimal>>()
+                ["philippines"] = new Dictionary<StayLength, Dictionary<Age, decimal>>()
                 {
-                    [StayLength.Short] = new Dictionary<string, decimal>()
+                    [StayLength.Short] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 42.99m,
-                        ["child"] = 39.99m
+                        [Age.Adult] = 42.99m,
+                        [Age.Child] = 39.99m
                     },
-                    [StayLength.Medium] = new Dictionary<string, decimal>()
+                    [StayLength.Medium] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 41m,
-                        ["child"] = 36m
+                        [Age.Adult] = 41m,
+                        [Age.Child] = 36m
                     },
-                    [StayLength.Long] = new Dictionary<string, decimal>()
+                    [StayLength.Long] = new Dictionary<Age, decimal>()
                     {
-                        ["oldfart"] = 38.50m,
-                        ["child"] = 32.40m
+                        [Age.Adult] = 38.50m,
+                        [Age.Child] = 32.40m
                     }
                 }
             };
 
-            var transportationMap = new Dictionary<string, Dictionary<string, decimal>>()
+            var transportationMap = new Dictionary<string, Dictionary<Age, decimal>>()
             {
-                ["train"] = new Dictionary<string, decimal>()
+                ["train"] = new Dictionary<Age, decimal>()
                 {
-                    ["oldfart"] = 22.3m,
-                    ["child"] = 12.5m
+                    [Age.Adult] = 22.3m,
+                    [Age.Child] = 12.5m
                 },
-                ["bus"] = new Dictionary<string, decimal>()
+                ["bus"] = new Dictionary<Age, decimal>()
                 {
-                    ["oldfart"] = 45m,
-                    ["child"] = 37m
+                    [Age.Adult] = 45m,
+                    [Age.Child] = 37m
                 },
-                ["airplane"] = new Dictionary<string, decimal>()
+                ["airplane"] = new Dictionary<Age, decimal>()
                 {
-                    ["oldfart"] = 90m,
-                    ["child"] = 68.50m
+                    [Age.Adult] = 90m,
+                    [Age.Child] = 68.50m
                 },
             };
 
@@ -95,11 +97,11 @@ namespace the_ivanov_family_holiday
             StayLength stay = ConvertNumberOfNightsToStayLength(numberOfNights);
 
             decimal transportationCost =
-            (transportationMap[transport]["child"] * 3)
-            + (transportationMap[transport]["oldfart"] * 2);
+            (transportationMap[transport][Age.Child] * 3)
+            + (transportationMap[transport][Age.Adult] * 2);
 
             decimal stayCost = numberOfNights *
-            ((destinationMap[destination][stay]["child"] * 3) + (destinationMap[destination][stay]["oldfart"] * 2))
+            ((destinationMap[destination][stay][Age.Child] * 3) + (destinationMap[destination][stay][Age.Adult] * 2))
             * 1.25m;
 
             decimal expense = transportationCost + stayCost;
