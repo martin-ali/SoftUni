e0a428f74ff2f2d9a3e66a122b86b1d0981a941d
Tectonik <marto7395@gmail.com>
2018-03-05 22:22:57 +0200

    Solved "Currency check"

---
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/currency-check/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace currency_check
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal rubles = (decimal.Parse(Console.ReadLine()) / 100) * 3.5m;
+            decimal dollars = decimal.Parse(Console.ReadLine()) * 1.5m;
+            decimal euros = decimal.Parse(Console.ReadLine()) * 1.95m;
+            decimal levaA = decimal.Parse(Console.ReadLine()) / 2;
+            decimal levaB = decimal.Parse(Console.ReadLine());
+
+            decimal cheapestOption = new decimal[] { rubles, dollars, euros, levaA, levaB }.Min();
+            Console.WriteLine($"{cheapestOption:0.00}");
+        }
+    }
+}
