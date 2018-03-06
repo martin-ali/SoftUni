720541d2e044a6776772d2763fd51b505b7c2e7c
Tectonik <marto7395@gmail.com>
2018-03-06 23:57:21 +0200

    Initialized "Magic wand"

---
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/magic-wand/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace magic_wand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
