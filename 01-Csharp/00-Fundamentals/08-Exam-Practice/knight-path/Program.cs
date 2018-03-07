383048442cc9e6db8c949369c51b34265cd6bee6
Tectonik <marto7395@gmail.com>
2018-03-07 22:28:51 +0200

    Solved "Knight path"

--- 01-Csharp/00-Fundamentals/08-Exam-Practice/knight-path/Program.cs
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/knight-path/Program.cs
@@ -8,100 +8,111 @@ namespace knight_path
         // Tuples da shit, mun!
         // Should be careful and avoid turning my code into JavaScript with Anonymous Types and Tuples
         // Dynamic remains useless
-        private static Dictionary<string, (int x, int y)> directions = new Dictionary<string, (int x, int y)>
+        private static Dictionary<string, (int row, int col)> directions = new Dictionary<string, (int row, int col)>
         {
-            ["left up"] = (-2, -1),
-            ["left down"] = (-2, 1),
-            ["right up"] = (2, -1),
-            ["right down"] = (2, 1),
-            ["up left"] = (-1, -2),
-            ["up right"] = (1, -2),
-            ["down left"] = (-1, 2),
-            ["down right"] = (1, 2)
+            ["left up"] = (-1, 2),
+            ["left down"] = (1, 2),
+            ["right up"] = (-1, -2),
+            ["right down"] = (1, -2),
+
+            ["up left"] = (-2, 1),
+            ["up right"] = (-2, -1),
+            ["down left"] = (2, 1),
+            ["down right"] = (2, -1),
+
+            // Avoid this?
+            ["stop"] = (0, 0)
         };
 
         static void Main()
         {
-            var moves = new List<string>()
-            {
-                "left down",
-                "down right",
-                "right up",
-                "down right",
-                "left down",
-                "left down"
-            };
-            var board = new bool[8, 8];
-            var currentKnightX = 7;
-            var currentKnightY = 0;
-
-            // while (true)
+            // Whole thing could be done with bitwise operations, but I avoided that on purpose. 
+            // I just like doing things my own way when I'm learning
+            // var moves = new List<string>()
             // {
-            //     var command = Console.ReadLine().ToLower();
+            //     "left down",
+            //     "down right",
+            //     "right up",
+            //     "down right",
+            //     "left down",
+            //     "left down",
+            //     "stop"
+            // };
 
-            //     if (command == "stop")
-            //     {
-            //         break;
-            //     }
-            //     else
-            //     {
-            //         moves.Add(command);
-            //     }
-            // }
-
-            // board[0, 7] = true;
-            foreach (string move in moves)
-            {
-                var newCoordinates = ExecuteOnBoard(board, move, currentKnightX, currentKnightY);
-                currentKnightX = newCoordinates.x;
-                currentKnightY = newCoordinates.y;
-            }
-
-            // Print board
+            var board = new bool[8, 8];
+            var moves = FetchCommandsFromConsole();
+            ExecuteCommands(moves, board);
             PrintGameBoard(board);
         }
 
+        private static List<string> FetchCommandsFromConsole()
+        {
+            var moves = new List<string>();
+            while (true)
+            {
+                var move = Console.ReadLine().ToLower();
+                moves.Add(move);
+
+                if (move == "stop") break;
+            }
+
+            return moves;
+        }
+
+        private static void ExecuteCommands(List<string> moves, bool[,] board)
+        {
+            var currentKnightRow = 0;
+            var currentKnightCol = 0;
+
+            foreach (string move in moves)
+            {
+                var newCoordinates = ExecuteCommand(board, move, currentKnightRow, currentKnightCol);
+                currentKnightRow = newCoordinates.x;
+                currentKnightCol = newCoordinates.y;
+            }
+        }
+
+        // The board is technically inverted on its horizontal axis, but I can't find a reason to fix it
         private static void PrintGameBoard(bool[,] board)
         {
-            var shit = false;
+            var boardIsEmpty = false;
             for (int row = 0; row < 8; row++)
             {
-                double number = 0;
-                for (int col = 7; col >= 0; col--)
+                double num = 0;
+                for (int col = 0; col < 8; col++)
                 {
+                    // Console.Write((board[row, col] ? "x" : "_") + " ");
                     if (board[row, col])
                     {
-                        number += Math.Pow(2, col - 7);
+                        num += Math.Pow(2, col);
                     }
                 }
 
-                if (number != 0)
+                if (num != 0)
                 {
-                    shit = true;
-                    Console.WriteLine(number);
+                    Console.WriteLine(num);
+                    boardIsEmpty = true;
                 }
             }
 
-            if (shit)
+            if (boardIsEmpty == false)
             {
                 Console.WriteLine("[Board is empty]");
             }
         }
 
-        static (int x, int y) ExecuteOnBoard(bool[,] board, string command, int currentX, int currentY)
+        static (int x, int y) ExecuteCommand(bool[,] board, string command, int currentRow, int currentCol)
         {
-            var commandParts = command.Split(" ");
-
             var nextMove = directions[command];
-            var newX = currentX + nextMove.x;
-            var newY = currentY + nextMove.y;
+            var newRow = currentRow + nextMove.row;
+            var newCol = currentCol + nextMove.col;
 
-            var newCoordinates = (currentX, currentY);
-            bool movementIsValid = 0 <= newX && newX <= 7 && 0 <= newY && newY <= 7;
+            var newCoordinates = (currentRow, currentCol);
+            bool movementIsValid = (0 <= newRow && newRow < board.GetLength(0)) && (0 <= newCol && newCol < board.GetLength(0));
             if (movementIsValid)
             {
-                board[currentX, currentY] = !board[currentX, currentY];
-                newCoordinates = (newX, newY);
+                board[currentRow, currentCol] = !board[currentRow, currentCol];
+                newCoordinates = (newRow, newCol);
             }
 
             return newCoordinates;
