f80ac89fd8d7137a82c003c0b8399424054e8101
Tectonik <marto7395@gmail.com>
2018-03-10 13:14:16 +0200

    Solved "Two numbers sum"

--- 01-Csharp/00-Fundamentals/08-Exam-Practice/two-numbers-sum/Program.cs
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/two-numbers-sum/Program.cs
@@ -4,9 +4,42 @@ namespace two_numbers_sum
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int intervalStart = int.Parse(Console.ReadLine());
+            int intervalEnd = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            // int intervalStart = 10;
+            // int intervalEnd = 2;
+            // int magicNumber = 2000;
+
+            int maxComboCount = intervalStart - intervalEnd + 1;
+            maxComboCount *= maxComboCount;
+
+            int currentCombination = 0;
+            bool CombinationIsFound = false;
+
+            for (int i = intervalStart; i >= intervalEnd; i--)
+            {
+                for (int j = intervalStart; j >= intervalEnd; j--)
+                {
+                    currentCombination++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{currentCombination} ({i} + {j} = {magicNumber})");
+                        i = intervalEnd - 1; // Break outer loop as well
+                        CombinationIsFound = true;
+                        break;
+                    }
+                }
+            }
+
+            // This isn't tested against?
+            if (CombinationIsFound == false)
+            {
+                Console.WriteLine($"{maxComboCount} combinations - neither equals {magicNumber}");
+            }
         }
     }
 }
