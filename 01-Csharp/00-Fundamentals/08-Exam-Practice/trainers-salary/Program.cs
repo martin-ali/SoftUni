26739a8a8aa0d9969a3dacac7004fd719d408895
Tectonik <marto7395@gmail.com>
2018-03-10 17:07:07 +0200

    Solved "Trainers salary"

--- 01-Csharp/00-Fundamentals/08-Exam-Practice/trainers-salary/Program.cs
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/trainers-salary/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace trainers_salary
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int numberOfLectures = int.Parse(Console.ReadLine());
+            decimal budget = decimal.Parse(Console.ReadLine());
+            var woot = new Dictionary<string, decimal>
+            {
+                ["Jelev"] = 0,
+                ["RoYaL"] = 0,
+                ["Roli"] = 0,
+                ["Trofon"] = 0,
+                ["Sino"] = 0,
+                ["Others"] = 0
+            };
+
+            for (int i = 0; i < numberOfLectures; i++)
+            {
+                var trainer = Console.ReadLine();
+                if (woot.ContainsKey(trainer))
+                {
+                    woot[trainer] += budget / numberOfLectures; ;
+                }
+                else
+                {
+                    woot["Others"] += budget / numberOfLectures;
+                }
+            }
+
+            foreach (var trainer in woot)
+            {
+                Console.WriteLine($"{trainer.Key} salary: {trainer.Value:0.00} lv");
+            }
         }
     }
 }
