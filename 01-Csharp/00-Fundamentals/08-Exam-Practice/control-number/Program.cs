c947b4f7ce81f6b817e6947efac6a69ad733e4d5
Tectonik <marto7395@gmail.com>
2018-03-11 11:33:31 +0200

    Initialized "Control number" as warmup before exam

---
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/control-number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace control_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
