af93bf32f26c9ea625a8ef5040a2cc4067413298
Tectonik <marto7395@gmail.com>
2018-03-11 14:28:45 +0200

    WTH, Git?

--- 01-Csharp/00-Fundamentals/08-Exam-Practice/control-number/Program.cs
+++ 01-Csharp/00-Fundamentals/08-Exam-Practice/control-number/Program.cs
@@ -4,9 +4,38 @@ namespace control_number
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int controlNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            var sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = m; j >= 1; j--)
+                {
+                    counter++;
+                    sum += (i * 2);
+                    sum += (j * 3);
+                    if (sum >= controlNumber)
+                    {
+                        i = n + 1;
+                        break;
+                    }
+                }
+            }
+
+            if (sum >= controlNumber)
+            {
+                Console.WriteLine($"{counter} moves");
+                Console.WriteLine($"Score: {sum} >= {controlNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{counter} moves");
+            }
         }
     }
 }
