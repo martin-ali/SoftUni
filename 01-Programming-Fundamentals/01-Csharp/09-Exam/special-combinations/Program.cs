e1838ad8bb8d9f338b50173d4f56eef4a1045243
Tectonik <marto7395@gmail.com>
2018-03-15 14:21:50 +0200

    Solved "Special combinations"

---
+++ 01-Programming-Fundamentals/01-Csharp/09-Exam/special-combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace special_combinations
+{
+    class Program
+    {
+        static void Main()
+        {
+            int hundredsUpperCeiling = int.Parse(Console.ReadLine());
+            int tensUpperCeiling = int.Parse(Console.ReadLine());
+            int singlesUpperCeiling = int.Parse(Console.ReadLine());
+            int[] firstPrimes = new int[] { 2, 3, 5, 7 };
+
+            for (int hundreds = 2; hundreds <= hundredsUpperCeiling; hundreds += 2)
+            {
+                for (int j = 0; j < 4 && firstPrimes[j] <= tensUpperCeiling; j++)
+                {
+                    var tens = firstPrimes[j];
+                    for (int singles = 2; singles <= singlesUpperCeiling; singles += 2)
+                    {
+                        Console.WriteLine($"{hundreds} {tens} {singles}");
+                    }
+                }
+            }
+        }
+    }
+}
