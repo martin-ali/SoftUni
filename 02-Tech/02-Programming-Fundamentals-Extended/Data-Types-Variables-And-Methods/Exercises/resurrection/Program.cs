6cf69b076fe773b8eefd51c3ac41c9916ab8ac6c
Tectonik <marto7395@gmail.com>
2018-03-15 23:34:33 +0200

    Solved "Resurrection"

--- 02-Tech/02-Programming-Fundamentals-Extended/Data-Types-Variables-And-Methods/Exercises/resurrection/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/Data-Types-Variables-And-Methods/Exercises/resurrection/Program.cs
@@ -4,9 +4,21 @@ namespace resurrection
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int numberOfPhoenixes = int.Parse(Console.ReadLine());
+
+            while (numberOfPhoenixes-- > 0)
+            {
+                var bodyLength = long.Parse(Console.ReadLine());
+                var bodyWidth = decimal.Parse(Console.ReadLine());
+                var wingLength = long.Parse(Console.ReadLine());
+
+                var phoenix = (length: bodyLength, width: bodyWidth, wingLength: wingLength);
+                var yearsToReincarnate = (phoenix.length * phoenix.length) * (phoenix.width + (2 * phoenix.wingLength));
+
+                Console.WriteLine(yearsToReincarnate);
+            }
         }
     }
 }
