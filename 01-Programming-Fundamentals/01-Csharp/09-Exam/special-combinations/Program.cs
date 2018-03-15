3b0a89e39ee2e0ff3ca98d36d6985675499b80cb
Tectonik <marto7395@gmail.com>
2018-03-15 20:37:30 +0200

    Solved "Anonymous Downsite"

--- 01-Programming-Fundamentals/01-Csharp/09-Exam/special-combinations/Program.cs
+++ 01-Programming-Fundamentals/01-Csharp/09-Exam/special-combinations/Program.cs
@@ -9,13 +9,13 @@ namespace special_combinations
             int hundredsUpperCeiling = int.Parse(Console.ReadLine());
             int tensUpperCeiling = int.Parse(Console.ReadLine());
             int singlesUpperCeiling = int.Parse(Console.ReadLine());
-            int[] firstPrimes = new int[] { 2, 3, 5, 7 };
+            int[] primes = new int[] { 2, 3, 5, 7 };
 
             for (int hundreds = 2; hundreds <= hundredsUpperCeiling; hundreds += 2)
             {
-                for (int j = 0; j < 4 && firstPrimes[j] <= tensUpperCeiling; j++)
+                for (int j = 0; j < 4 && primes[j] <= tensUpperCeiling; j++)
                 {
-                    var tens = firstPrimes[j];
+                    var tens = primes[j];
                     for (int singles = 2; singles <= singlesUpperCeiling; singles += 2)
                     {
                         Console.WriteLine($"{hundreds} {tens} {singles}");
--- 02-Tech/02-Programming-Fundamentals/Data-Types-Variables-And-Methods/Exercises/anonymous-downsite/Program.cs
+++ 02-Tech/02-Programming-Fundamentals/Data-Types-Variables-And-Methods/Exercises/anonymous-downsite/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Numerics;
 
 namespace anonymous_downsite
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            byte numberOfDownedWebsites = byte.Parse(Console.ReadLine());
+            byte securityKey = byte.Parse(Console.ReadLine());
+            decimal totalLosses = 0;
+
+            for (long current = 0; current < numberOfDownedWebsites; current++)
+            {
+                var site = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                totalLosses += long.Parse(site[1]) * decimal.Parse(site[2]);
+                Console.WriteLine(site[0]);
+            }
+
+            Console.WriteLine($"Total Loss: {totalLosses:f20}");
+            Console.WriteLine($"Security Token: {BigInteger.Pow(securityKey, numberOfDownedWebsites)}");
         }
     }
 }
