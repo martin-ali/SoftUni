266513c975dbaeaacbbb2be23b361f1b5de910cd
Tectonik <marto7395@gmail.com>
2018-03-16 00:00:58 +0200

    Solved "Wormtest"

--- 02-Tech/02-Programming-Fundamentals-Extended/Data-Types-Variables-And-Methods/Exercises/wormtest/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/Data-Types-Variables-And-Methods/Exercises/wormtest/Program.cs
@@ -4,9 +4,24 @@ namespace wormtest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            long length = long.Parse(Console.ReadLine()) * 100;
+            decimal width = decimal.Parse(Console.ReadLine());
+
+            var remainder = (length != 0 && width != 0)
+                                ? length % width
+                                : 0;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine($"{length * width:0.00}");
+            }
+            else
+            {
+                decimal result = 1 / (width / length) * 100;
+                Console.WriteLine($"{result:0.00}%");
+            }
         }
     }
 }
