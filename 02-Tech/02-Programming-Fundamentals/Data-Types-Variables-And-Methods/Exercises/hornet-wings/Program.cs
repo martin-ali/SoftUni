ad6ebd526b0fbda783e00f6a804b928dba1e653c
Tectonik <marto7395@gmail.com>
2018-03-15 21:44:37 +0200

    Solved "Hornet Wings"

--- 02-Tech/02-Programming-Fundamentals/Data-Types-Variables-And-Methods/Exercises/hornet-wings/Program.cs
+++ 02-Tech/02-Programming-Fundamentals/Data-Types-Variables-And-Methods/Exercises/hornet-wings/Program.cs
@@ -4,9 +4,18 @@ namespace hornet_wings
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int wingflaps = int.Parse(Console.ReadLine());
+            double distancePer1000Flaps = double.Parse(Console.ReadLine());
+            int endurance = int.Parse(Console.ReadLine());
+
+            var restTime = (wingflaps / endurance) * 5;
+            var distanceTraveled = (wingflaps / 1000) * distancePer1000Flaps;
+            var time = wingflaps / 100;
+
+            Console.WriteLine($"{distanceTraveled:0.00} m.");
+            Console.WriteLine($"{time+restTime} s.");
         }
     }
 }
