e0fff2ab9de1cf6064af4bce76f5fa4088b7b4fc
Tectonik <marto7395@gmail.com>
2018-03-15 23:17:47 +0200

    Solved "Poke Mon"

--- 02-Tech/02-Programming-Fundamentals/Data-Types-Variables-And-Methods/Exercises/poke-mon/Program.cs
+++ 02-Tech/02-Programming-Fundamentals/Data-Types-Variables-And-Methods/Exercises/poke-mon/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Numerics;
 
 namespace poke_mon
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int pokePowerStart = int.Parse(Console.ReadLine());
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+
+            int pokePower = pokePowerStart;
+            int targetsPoked = 0;
+
+            while (pokePower >= distanceBetweenTargets)
+            {
+                pokePower -= distanceBetweenTargets;
+                targetsPoked++;
+
+                if (pokePower * 2 == pokePowerStart && exhaustionFactor > 0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetsPoked);
         }
     }
 }
