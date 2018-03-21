42e9adb3450eb1d1b2e9d29602e0987394bceda3
Tectonik <marto7395@gmail.com>
2018-03-21 22:42:21 +0200

    Solved "Pokemon don't go"

--- 02-Tech/02-Programming-Fundamentals-Extended/02-Arrays-And-Lists/Exercises/03-pokemon-dont-go/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/02-Arrays-And-Lists/Exercises/03-pokemon-dont-go/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03_pokemon_dont_go
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var pokemons = Console
+                            .ReadLine()
+                            .Split(new char[] { ' ' }, StringSplitOptions.None)
+                            .Select(long.Parse).ToList();
+            var sum = 0l;
+
+            while (pokemons.Count > 0)
+            {
+                var index = int.Parse(Console.ReadLine());
+
+                long currentPokemon;
+                if (index < 0)
+                {
+                    currentPokemon = pokemons[0];
+                    pokemons[0] = pokemons[pokemons.Count - 1];
+                }
+                else if (index >= pokemons.Count)
+                {
+                    currentPokemon = pokemons[pokemons.Count - 1];
+                    pokemons[pokemons.Count - 1] = pokemons[0];
+                }
+                else
+                {
+                    currentPokemon = pokemons[index];
+                    pokemons.RemoveAt(index);
+                }
+
+                sum += currentPokemon;
+                for (int i = 0; i < pokemons.Count; i++)
+                {
+                    if (pokemons[i] <= currentPokemon)
+                    {
+                        pokemons[i] += currentPokemon;
+                    }
+                    else
+                    {
+                        pokemons[i] -= currentPokemon;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
