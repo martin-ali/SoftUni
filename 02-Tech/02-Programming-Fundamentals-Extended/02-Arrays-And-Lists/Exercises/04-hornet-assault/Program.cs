ec9b93f927b6f3f5290a1fb3c11a50a098f286f5
Tectonik <marto7395@gmail.com>
2018-03-21 23:12:17 +0200

    Solved "Hornet assault"

--- 02-Tech/02-Programming-Fundamentals-Extended/02-Arrays-And-Lists/Exercises/04-hornet-assault/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/02-Arrays-And-Lists/Exercises/04-hornet-assault/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04_hornet_assault
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var beehives = Console
+                            .ReadLine()
+                            .Split(new char[] { ' ' }, StringSplitOptions.None)
+                            .Select(long.Parse)
+                            .ToList();
+            var hornets = Console
+                            .ReadLine()
+                            .Split(new char[] { ' ' }, StringSplitOptions.None)
+                            .Select(long.Parse)
+                            .ToList();
+
+            for (int current = 0; current < beehives.Count; current++)
+            {
+                var hornetsPower = hornets.Sum();
+
+                if (beehives[current] >= hornetsPower && hornets.Count > 0)
+                {
+                    hornets.RemoveAt(0);
+                }
+
+                beehives[current] -= hornetsPower;
+            }
+
+            var survivorHives = beehives.Where(x => x > 0);
+            if (survivorHives.Count() > 0)
+            {
+                Console.WriteLine(String.Join(" ", survivorHives));
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", hornets));
+            }
         }
     }
 }
