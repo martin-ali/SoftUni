5ad55585307bd6d242eac93459ae3f39cf05326e
Tectonik <marto7395@gmail.com>
2018-03-21 19:14:24 +0200

    Solved "Icarus"

--- 02-Tech/02-Programming-Fundamentals-Extended/02-Arrays-And-Lists/Exercises/02-icarus/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/02-Arrays-And-Lists/Exercises/02-icarus/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02_icarus
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var damage = 1;
+            var plane = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            var icarusPosition = int.Parse(Console.ReadLine());
+
+            var commandRaw = Console.ReadLine().ToLower();
+            while (commandRaw != "supernova")
+            {
+                var commandData = commandRaw.Split(new char[] { ' ' }, StringSplitOptions.None);
+                var command = (direction: commandData[0], steps: int.Parse(commandData[1]));
+
+                if (command.direction == "left")
+                {
+                    for (int count = 0; count < command.steps; count++)
+                    {
+                        icarusPosition--;
+                        if (icarusPosition < 0)
+                        {
+                            icarusPosition = plane.Length - 1;
+                            damage++;
+                        }
+
+                        plane[icarusPosition] -= damage;
+                    }
+                }
+                else
+                {
+                    for (int count = 0; count < command.steps; count++)
+                    {
+                        icarusPosition++;
+                        if (icarusPosition >= plane.Length)
+                        {
+                            icarusPosition = 0;
+                            damage++;
+                        }
+
+                        plane[icarusPosition] -= damage;
+                    }
+                }
+
+
+                commandRaw = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine(String.Join(" ", plane));
         }
     }
 }
