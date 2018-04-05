361d2278c37678862320984a35abdff8b664a0dc
Tectonik <marto7395@gmail.com>
2018-04-05 21:26:57 +0300

    Initialized "Strings and Regex" exercises

---
+++ 02-Tech/02-Programming-Fundamentals-Extended/03-Strings-And-Regex/Exercises/03-hornet-comm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_hornet_comm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
