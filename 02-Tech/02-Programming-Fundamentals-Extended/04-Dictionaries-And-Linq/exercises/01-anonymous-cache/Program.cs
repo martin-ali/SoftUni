d00180cc17bf1acb53cfb42feb62df704b0ac70d
Tectonik <marto7395@gmail.com>
2018-04-09 20:21:08 +0300

    Initialized PFE "Dictionaries and LINQ" exercises

---
+++ 02-Tech/02-Programming-Fundamentals-Extended/04-Dictionaries-And-Linq/exercises/01-anonymous-cache/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_anonymous_cache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 02-Tech/02-Programming-Fundamentals-Extended/04-Dictionaries-And-Linq/exercises/03-code-phoenix-oscar-romeo-november/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03_code_phoenix_oscar_romeo_november
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
