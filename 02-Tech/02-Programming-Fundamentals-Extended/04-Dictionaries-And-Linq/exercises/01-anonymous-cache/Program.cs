f2e148f149366a793c53469014ffd735b2d55514
Tectonik <marto7395@gmail.com>
2018-04-11 00:07:49 +0300

    Working on PFE "Dictionaries and LINQ" exercises

--- 02-Tech/02-Programming-Fundamentals-Extended/04-Dictionaries-And-Linq/exercises/01-anonymous-cache/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/04-Dictionaries-And-Linq/exercises/01-anonymous-cache/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01_anonymous_cache
 {
+    public static class Extensions
+    {
+        public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out T third)
+        {
+
+            first = list.Count > 0 ? list[0] : default(T);
+            second = list.Count > 1 ? list[1] : default(T);
+            third = list.Count > 2 ? list[2] : default(T);
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var data = new Dictionary<string, Dictionary<string, long>>();
+            var cache = new Dictionary<string, Dictionary<string, long>>();
+
+            var line = Console.ReadLine();
+            while (line != "thetinggoesskrra")
+            {
+                if (line.Contains("->"))
+                {
+                    var (dataKey, dataSize, dataSet) = line.Split(new char[] { '-', '>', ' ', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (data.ContainsKey(dataSet))
+                    {
+                        data[dataSet][dataKey] = long.Parse(dataSize);
+                    }
+                    else
+                    {
+                        if (cache.ContainsKey(dataSet) == false)
+                        {
+                            cache[dataSet] = new Dictionary<string, long>();
+                        }
+
+                        cache[dataSet][dataKey] = long.Parse(dataSize);
+                    }
+                }
+                else
+                {
+                    data[line] = new Dictionary<string, long>();
+
+                    if (cache.ContainsKey(line))
+                    {
+                        data[line] = cache[line];
+                        // cache.Remove(line);
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+
+            if (data.Count > 0)
+            {
+                // var biggestDataSet = data.Aggregate((setA, setB) => setA.Value.Values.Sum() > setB.Value.Values.Sum() ? setA : setB);
+                var biggestDataSet = data.OrderByDescending(x => x.Value.Values.Sum()).First();
+                Console.WriteLine($"Data Set: {biggestDataSet.Key}, Total Size: {biggestDataSet.Value.Values.Sum()}");
+
+                var dataKeys = biggestDataSet.Value.Keys;
+                foreach (var dataKey in dataKeys)
+                {
+                    Console.WriteLine($"$.{dataKey}");
+                }
+            }
         }
     }
 }
--- 02-Tech/02-Programming-Fundamentals-Extended/04-Dictionaries-And-Linq/exercises/02-hornet-armada/Program.cs
+++ 02-Tech/02-Programming-Fundamentals-Extended/04-Dictionaries-And-Linq/exercises/02-hornet-armada/Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02_hornet_armada
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int index = 0;
+
+        private static string[] testInput = @"6
+1 = BlackBeatles -> Soldier:2000
+2 = BlackBeatles -> Worker:1000
+1 = Red_Ones -> Soldier:10000
+5 = Rm -> Soldier:30000
+2 = Red_Ones -> Soldier:20000
+10 = RND -> Soldier:100000
+10\Soldier".Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var legions = new Dictionary<string, Dictionary<string, int>>();
+            var legionActivity = new Dictionary<string, int>();
+
+            // int numberOfLines = int.Parse(Console.ReadLine());
+            int numberOfLines = int.Parse(testInput[index++]);
+
+            while (numberOfLines-- > 0)
+            {
+                // var rawLine = Console.ReadLine();
+                var rawLine = testInput[index++];
+                var rawData = rawLine.Split(new char[] { '-', '>', ' ', '=', ':' }, StringSplitOptions.RemoveEmptyEntries);
+                var currentLastActivity = int.Parse(rawData[0]);
+                var legionName = rawData[1];
+                var soldierType = rawData[2];
+                var soldierCount = int.Parse(rawData[3]);
+
+                if (legions.ContainsKey(legionName) == false)
+                {
+                    legions[legionName] = new Dictionary<string, int>();
+                    legionActivity[legionName] = 0;
+                }
+
+                if (legions[legionName].ContainsKey(soldierType) == false)
+                {
+                    legions[legionName][soldierType] = 0;
+                }
+
+                legions[legionName][soldierType] += soldierCount;
+                legionActivity[legionName] = Math.Max(legionActivity[legionName], currentLastActivity);
+            }
+
+            // var lastLine = Console.ReadLine();
+            var lastLine = testInput[index++];
+            if (lastLine.Contains("\\"))
+            {
+                var rawInfo = lastLine.Split('\\');
+                var activity = int.Parse(rawInfo[0]);
+                var soldierType = rawInfo[1];
+
+                var rrrrrr = legions
+                .Where(l => l.Value.ContainsKey(soldierType) && legionActivity[l.Key] < activity)
+                .OrderByDescending(l => l.Value[soldierType]);
+
+                foreach (var item in rrrrrr)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value[soldierType]}");
+                }
+
+                // foreach (var legion in legions)
+                // {
+                //     foreach (var soldier in legion.Value)
+                //     {
+                //         Console.WriteLine(1);
+                //     }
+                // }
+            }
+            else
+            {
+
+            }
         }
     }
 }
