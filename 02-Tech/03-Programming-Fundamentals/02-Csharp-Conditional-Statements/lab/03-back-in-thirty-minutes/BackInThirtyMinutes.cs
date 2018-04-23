7b891ade6464f7f11a483836172e97d44c1c2166
Tectonik <marto7395@gmail.com>
2018-04-23 19:24:54 +0300

    Added an alternative solution for "Back in 30 minutes"

--- 02-Tech/03-Programming-Fundamentals/02-Csharp-Conditional-Statements/lab/03-back-in-thirty-minutes/BackInThirtyMinutes.cs
+++ 02-Tech/03-Programming-Fundamentals/02-Csharp-Conditional-Statements/lab/03-back-in-thirty-minutes/BackInThirtyMinutes.cs
@@ -7,21 +7,34 @@ namespace _03_back_in_30_minutes
         // Alternative: turn everything to minutes first
         static void Main()
         {
-            int hours = int.Parse(Console.ReadLine());
-            int minutes = int.Parse(Console.ReadLine()) + 30;
+            // Alternative solution
+            int startHours = int.Parse(Console.ReadLine());
+            int startMinutes = int.Parse(Console.ReadLine());
 
-            if (minutes > 59)
-            {
-                hours++;
-                minutes -= 60;
-            }
+            int totalMinutes = (startHours * 60) + startMinutes + 30;
 
-            if (hours > 23)
-            {
-                hours -= 24;
-            }
+            var hours = totalMinutes / 60;
+            var minutes = totalMinutes - (hours * 60);
+            if (hours > 23) hours -= 24;
 
             Console.WriteLine($"{hours}:{minutes:00}");
+
+            // Default solution
+            // int hours = int.Parse(Console.ReadLine());
+            // int minutes = int.Parse(Console.ReadLine()) + 30;
+
+            // if (minutes > 59)
+            // {
+            //     hours++;
+            //     minutes -= 60;
+            // }
+
+            // if (hours > 23)
+            // {
+            //     hours -= 24;
+            // }
+
+            // Console.WriteLine($"{hours}:{minutes:00}");
         }
     }
 }
