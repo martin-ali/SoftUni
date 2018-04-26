9ed905ef5b86aacaeffa76b4e61afd5dcb38f0e0
Tectonik <marto7395@gmail.com>
2018-04-26 22:33:12 +0300

    Solved "Tourist Information"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/04-tourist-information/TouristInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_tourist_information
+{
+    class TouristInformation
+    {
+
+        private static Dictionary<string, double> conversionTable = new Dictionary<string, double>
+        {
+            ["miles"] = 1.6,
+            ["inches"] = 2.54,
+            ["feet"] = 30,
+            ["yards"] = 0.91,
+            ["gallons"] = 3.8
+        };
+
+        private static Dictionary<string, string> metricUnitCounterpart = new Dictionary<string, string>
+        {
+            ["miles"] = "kilometers",
+            ["inches"] = "centimeters",
+            ["feet"] = "centimeters",
+            ["yards"] = "meters",
+            ["gallons"] = "liters"
+        };
+
+        static void Main()
+        {
+            var imperialUnit = Console.ReadLine();
+            double valueInImperial = double.Parse(Console.ReadLine());
+            var valueInMetric = valueInImperial * conversionTable[imperialUnit];
+            Console.WriteLine($"{valueInImperial} {imperialUnit} = {valueInMetric:0.00} {metricUnitCounterpart[imperialUnit]}");
+        }
+    }
+}
