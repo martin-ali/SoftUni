973ee31f5622970e17d1711470eecdd046339330
Tectonik <marto7395@gmail.com>
2018-04-27 23:28:38 +0300

    Finished "Thea The Photgrapher"

--- 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/exercises/19-thea-the-photographer/TheThePhotographer.cs
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/exercises/19-thea-the-photographer/TheThePhotographer.cs
@@ -4,9 +4,20 @@ namespace _19_thea_the_photographer
 {
     class TheThePhotographer
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            long numberOfPicturesTaken = long.Parse(Console.ReadLine());
+            long filterTime = long.Parse(Console.ReadLine());
+            double percentageOfGoodPictures = long.Parse(Console.ReadLine()) / 100.0;
+            long uploadTimePerFilteredPicture = long.Parse(Console.ReadLine());
+
+            var timeTakenToFilter = numberOfPicturesTaken * filterTime;
+            var filteredImages = Math.Ceiling(numberOfPicturesTaken * percentageOfGoodPictures);
+            var timeTakenToUploadPictures = filteredImages * uploadTimePerFilteredPicture;
+
+            long secondsTaken = (long)(timeTakenToFilter + timeTakenToUploadPictures);
+            var timeTaken = TimeSpan.FromSeconds(secondsTaken);
+            Console.WriteLine(timeTaken.ToString("d\\:hh\\:mm\\:ss"));
         }
     }
 }
