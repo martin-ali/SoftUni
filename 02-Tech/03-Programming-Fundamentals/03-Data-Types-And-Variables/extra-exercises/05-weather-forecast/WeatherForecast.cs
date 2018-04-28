e98cb4a31c82d7d55dd442153d53f4e4cc874e56
Tectonik <marto7395@gmail.com>
2018-04-28 23:37:52 +0300

    Finished "Weather Forecast"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/05-weather-forecast/WeatherForecast.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05_weather_forecast
+{
+    class WeatherForecast
+    {
+        static void Main()
+        {
+            var numberString = Console.ReadLine();
+
+            var number = double.Parse(numberString);
+            var weather = string.Empty;
+
+            // Check if number is floating-point
+            if (number % 1 != 0)
+            {
+                weather = "Rainy";
+            }
+            else if (sbyte.MinValue <= number && number <= sbyte.MaxValue)
+            {
+                weather = "Sunny";
+            }
+            else if (int.MinValue <= number && number <= int.MaxValue)
+            {
+                weather = "Cloudy";
+            }
+            else if (long.MinValue <= number && number <= long.MaxValue)
+            {
+                weather = "Windy";
+            }
+
+            Console.WriteLine(weather);
+        }
+    }
+}
