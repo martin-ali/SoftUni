b6cacf1ea0ebec73bebae83643176cb932c1896b
Tectonik <marto7395@gmail.com>
2018-04-28 23:38:13 +0300

    Finished "Catch the Thief"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/06-catch-the-thief/CatchTheThief.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _06_catch_the_thief
+{
+    class CatchTheThief
+    {
+        static void Main()
+        {
+            var numeralType = Console.ReadLine();
+            int numberOfIds = int.Parse(Console.ReadLine());
+
+            var ids = new long[numberOfIds];
+            for (int current = 0; current < numberOfIds; current++)
+            {
+                ids[current] = long.Parse(Console.ReadLine());
+            }
+
+            long maxValue = 0;
+            switch (numeralType)
+            {
+                case "sbyte": maxValue = sbyte.MaxValue; break;
+                case "int": maxValue = int.MaxValue; break;
+                case "long": maxValue = long.MaxValue; break;
+            }
+
+            var thiefId = ids
+                            .Where(id => id <= maxValue)
+                            .Max();
+
+            Console.WriteLine(thiefId);
+        }
+    }
+}
