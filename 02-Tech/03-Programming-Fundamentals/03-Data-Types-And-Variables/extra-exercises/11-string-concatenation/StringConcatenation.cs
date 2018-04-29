7d1d39897a55e1dbdd0e18a5eab181688e9247e0
Tectonik <marto7395@gmail.com>
2018-04-29 23:24:11 +0300

    Solved "String Concatenation"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/11-string-concatenation/StringConcatenation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace _11_string_concatenation
+{
+    class StringConcatenation
+    {
+        static void Main()
+        {
+            var delimiter = char.Parse(Console.ReadLine());
+            var lineParity = Console.ReadLine();
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            if (lineParity == "even") Console.ReadLine();
+
+            var builder = new StringBuilder();
+            for (int i = 0; i < numberOfLines / 2; i++)
+            {
+                // Get line we need
+                var current = Console.ReadLine();
+                builder.Append(current + delimiter);
+
+                // Waste next line since we don't need it
+                Console.ReadLine();
+            }
+
+            // Get rid of last delimiter
+            builder.Remove(builder.Length - 1, 1);
+            Console.WriteLine(builder);
+        }
+    }
+}
