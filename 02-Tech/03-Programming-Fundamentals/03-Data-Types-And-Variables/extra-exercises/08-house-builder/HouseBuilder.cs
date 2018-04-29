ec40b148c1dbec66887055d6a30c136f0616e6f0
Tectonik <marto7395@gmail.com>
2018-04-29 20:50:44 +0300

    Finished "House Builder"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/08-house-builder/HouseBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08_house_builder
+{
+    class HouseBuilder
+    {
+        static void Main()
+        {
+            var firstNumber = long.Parse(Console.ReadLine());
+            var secondNumber = long.Parse(Console.ReadLine());
+
+            var integerNumber = Math.Max(firstNumber, secondNumber);
+            var sbyteNumber = Math.Min(firstNumber, secondNumber);
+
+            var materialsPrice = (10 * integerNumber) + (4 * sbyteNumber);
+            Console.WriteLine(materialsPrice);
+        }
+    }
+}
