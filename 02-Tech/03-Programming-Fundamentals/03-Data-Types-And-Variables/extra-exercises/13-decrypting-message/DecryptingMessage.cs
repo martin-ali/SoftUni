b57d0d67c0da032e47caf441b8eeef5bdbaa1819
Tectonik <marto7395@gmail.com>
2018-04-30 00:12:55 +0300

    Finished "Decrypting Message"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/13-decrypting-message/DecryptingMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _13_decrypting_message
+{
+    class DecryptingMessage
+    {
+        static void Main()
+        {
+            var key = int.Parse(Console.ReadLine());
+            var numberOfLines = int.Parse(Console.ReadLine());
+            var decryptedMessageBuilder = new StringBuilder();
+            for (int current = 0; current < numberOfLines; current++)
+            {
+                var encryptedCharacter = char.Parse(Console.ReadLine());
+                decryptedMessageBuilder.Append((char)(encryptedCharacter + key));
+            }
+
+            Console.WriteLine(decryptedMessageBuilder);
+        }
+    }
+}
