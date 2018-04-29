05581abb31f9428414d3311c89c594a0bea24b96
Tectonik <marto7395@gmail.com>
2018-04-29 22:43:19 +0300

    Finished "Make a Word"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/09-make-a-word/makeAWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace _09_make_a_word
+{
+    class makeAWord
+    {
+        static void Main()
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            var wordBuilder = new StringBuilder();
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var character = Console.ReadLine();
+                wordBuilder.Append(character);
+            }
+
+            Console.WriteLine($"The word is: {wordBuilder}");
+        }
+    }
+}
