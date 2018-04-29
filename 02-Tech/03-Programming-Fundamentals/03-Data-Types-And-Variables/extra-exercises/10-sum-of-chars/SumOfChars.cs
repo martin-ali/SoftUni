2a6ca4e80db6ed971d90f2cf970a8d21c893e9f7
Tectonik <marto7395@gmail.com>
2018-04-29 22:58:36 +0300

    Finished "Sum of Chars"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/10-sum-of-chars/SumOfChars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10_sum_of_chars
+{
+    class SumOfChars
+    {
+        static void Main()
+        {
+            var sum = 0;
+            var numberOfLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var character = char.Parse(Console.ReadLine());
+                sum += character;
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
