ae6f7d8462f623277f0ec052739e01b8dc468b5b
Tectonik <marto7395@gmail.com>
2018-04-30 19:31:50 +0300

    Finished "Numbers in reversed order"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/04-numbers-in-reversed-order/NumbersInReversedOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04_numbers_in_reversed_order
+{
+    class NumbersInReversedOrder
+    {
+        static void Main()
+        {
+            var numberString = Console.ReadLine();
+            var reversedNumberString = Reverse(numberString);
+            Console.WriteLine(reversedNumberString);
+        }
+
+        private static string Reverse(string stringToReverse)
+        {
+            var reversedString = string.Empty;
+            for (int character = stringToReverse.Length - 1; character >= 0; character--)
+            {
+                reversedString += stringToReverse[character];
+            }
+
+            return reversedString;
+        }
+    }
+}
