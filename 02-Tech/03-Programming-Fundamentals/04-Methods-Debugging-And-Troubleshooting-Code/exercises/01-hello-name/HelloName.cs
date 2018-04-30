4360adedb336836eb0b89996400ffcd6a86ddb6c
Tectonik <marto7395@gmail.com>
2018-04-30 19:12:01 +0300

    Finished "Hello, Name!"

--- 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/01-hello-name/HelloName.cs
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/01-hello-name/HelloName.cs
@@ -6,7 +6,13 @@ namespace _01_hello_name
     {
         static void Main()
         {
-            
+            var name = Console.ReadLine();
+            PrintHello(name);
+        }
+
+        private static void PrintHello(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
         }
     }
 }
