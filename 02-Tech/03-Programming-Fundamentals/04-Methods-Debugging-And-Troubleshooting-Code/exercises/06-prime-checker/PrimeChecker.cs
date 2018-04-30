26287f31f537e600a0cc76713fd67ee12f576d4e
Tectonik <marto7395@gmail.com>
2018-04-30 20:53:56 +0300

    Finished "Prime checker"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/06-prime-checker/PrimeChecker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06_prime_checker
+{
+    class PrimeChecker
+    {
+        static void Main()
+        {
+            var number = long.Parse(Console.ReadLine());
+            Console.WriteLine(isPrime(number));
+        }
+
+        private static bool isPrime(long number)
+        {
+            if (number == 2)
+            {
+                return true;
+            }
+
+            if (number % 2 == 0 || number <= 1)
+            {
+                return false;
+            }
+
+            var sqrtNumber = Math.Sqrt(number);
+            for (int current = 3; current <= sqrtNumber; current += 2)
+            {
+                if (number % current == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
