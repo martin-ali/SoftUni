3e834e460c7d5c01f9904c5448b29bc93531c00e
Tectonik <marto7395@gmail.com>
2018-04-30 22:54:55 +0300

    Finished "Cube properties"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/10-cube-properties/CubeProperties.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _10_cube_properties
+{
+    class CubeProperties
+    {
+        static void Main()
+        {
+            var side = double.Parse(Console.ReadLine());
+            var parameter = Console.ReadLine();
+
+            var result = 0.0;
+            switch (parameter)
+            {
+                case "face":
+                    result = Math.Sqrt(2 * (side * side));
+                    break;
+                case "space":
+                    result = Math.Sqrt(3 * (side * side));
+                    break;
+                case "volume":
+                    result = Math.Pow(side, 3);
+                    break;
+                case "area":
+                    result = 6 * (side * side);
+                    break;
+            }
+
+            Console.WriteLine($"{result:0.00}");
+        }
+    }
+}
