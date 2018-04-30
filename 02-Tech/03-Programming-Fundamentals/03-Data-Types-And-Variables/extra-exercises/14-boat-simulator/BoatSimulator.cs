910dfa936dbdd5d5a00a24108fb6db917689edc0
Tectonik <marto7395@gmail.com>
2018-04-30 14:12:54 +0300

    Finished "Boat Simulator"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/14-boat-simulator/BoatSimulator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _14_boat_simulator
+{
+    class BoatSimulator
+    {
+        static void Main()
+        {
+            var firstBoatSymbol = char.Parse(Console.ReadLine());
+            var secondBoatSymbol = char.Parse(Console.ReadLine());
+            var numberOfTurns = int.Parse(Console.ReadLine());
+
+            var firstBoat = (symbol: firstBoatSymbol, speed: 0);
+            var secondBoat = (symbol: secondBoatSymbol, speed: 0);
+
+            for (int turn = 1; turn <= numberOfTurns; turn++)
+            {
+                var command = Console.ReadLine();
+                if (command == "UPGRADE")
+                {
+                    firstBoat.symbol += (char)3;
+                    secondBoat.symbol += (char)3;
+                    continue;
+                }
+
+                var speed = command.Length;
+
+                if (turn % 2 != 0)
+                {
+                    firstBoat.speed += speed;
+                    if (firstBoat.speed >= 50)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    secondBoat.speed += speed;
+                    if (secondBoat.speed >= 50)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var winner = firstBoat.speed > secondBoat.speed ? firstBoat : secondBoat;
+            Console.WriteLine(winner.symbol);
+        }
+    }
+}
