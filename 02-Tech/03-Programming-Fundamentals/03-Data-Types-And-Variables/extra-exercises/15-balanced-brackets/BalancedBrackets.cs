6df482b77d244387901b83cb99d006b50e733be7
Tectonik <marto7395@gmail.com>
2018-04-30 14:43:48 +0300

    Finished "Balanced Brackets"

---
+++ 02-Tech/03-Programming-Fundamentals/03-Data-Types-And-Variables/extra-exercises/15-balanced-brackets/BalancedBrackets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _15_balanced_brackets
+{
+    class BalancedBrackets
+    {
+        static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            var bracketAreBalanced = true;
+            var lastBracket = ")";
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var currentLine = Console.ReadLine();
+                if (currentLine != "(" && currentLine != ")")
+                {
+                    continue;
+                }
+
+                var currentBracket = currentLine;
+                if (currentBracket == lastBracket)
+                {
+                    bracketAreBalanced = false;
+                    break;
+                }
+
+                lastBracket = currentBracket;
+            }
+
+            var lastBracketIsWrong = lastBracket == "(";
+            var bracketsState = bracketAreBalanced && lastBracketIsWrong == false ? "BALANCED" : "UNBALANCED";
+            Console.WriteLine(bracketsState);
+        }
+    }
+}
