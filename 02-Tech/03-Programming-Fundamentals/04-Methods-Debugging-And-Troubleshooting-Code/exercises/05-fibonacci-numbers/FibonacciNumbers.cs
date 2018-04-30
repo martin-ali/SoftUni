b2e4e94a5465455b0888a6d5ec7c9ebd1598406e
Tectonik <marto7395@gmail.com>
2018-04-30 19:44:10 +0300

    Finished "Fibonacci numbers"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/05-fibonacci-numbers/FibonacciNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05_fibonacci_numbers
+{
+    class FibonacciNumbers
+    {
+        static void Main()
+        {
+            var fibonacciTarget = int.Parse(Console.ReadLine());
+            int fibonacci = GetFibonacci(fibonacciTarget);
+
+            Console.WriteLine(fibonacci);
+        }
+
+        private static int GetFibonacci(int fibonacciTarget)
+        {
+            var firstFibonacci = 1;
+            var secondFibonacci = 1;
+            var fibonacci = 1;
+
+            for (int current = 1; current < fibonacciTarget; current++)
+            {
+                fibonacci = firstFibonacci + secondFibonacci;
+
+                secondFibonacci = firstFibonacci;
+                firstFibonacci = fibonacci;
+            }
+
+            return fibonacci;
+        }
+    }
+}
