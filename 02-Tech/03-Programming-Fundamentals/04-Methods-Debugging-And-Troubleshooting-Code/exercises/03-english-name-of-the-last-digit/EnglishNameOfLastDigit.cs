8e78356d4e668e569e34c17a2796ad79b5812fe6
Tectonik <marto7395@gmail.com>
2018-04-30 19:26:27 +0300

    Finished "English Name of the Last Digit"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/03-english-name-of-the-last-digit/EnglishNameOfLastDigit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03_english_name_of_the_last_digit
+{
+    class EnglishNameOfLastDigit
+    {
+        static void Main()
+        {
+            var numberString = Console.ReadLine();
+
+            var lastDigit = int.Parse(numberString[numberString.Length - 1].ToString());
+            var digitInEnglish = GetDigitInEnglish(lastDigit);
+
+            Console.WriteLine(digitInEnglish);
+        }
+
+        private static string GetDigitInEnglish(int digit)
+        {
+            var digitNames = new string[]
+            {
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+            };
+
+            return digitNames[digit];
+        }
+    }
+}
