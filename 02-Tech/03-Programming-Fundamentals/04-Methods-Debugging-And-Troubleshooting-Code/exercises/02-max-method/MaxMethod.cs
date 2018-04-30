ecd5056c3ac3108f560dbd2c7ecd8942f9877b66
Tectonik <marto7395@gmail.com>
2018-04-30 19:16:48 +0300

    Finished "Max Method"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/02-max-method/MaxMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02_max_method
+{
+    class MaxMethod
+    {
+        static void Main()
+        {
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+
+            var biggestOfFirstTwo = GetMax(firstNumber, secondNumber);
+            var max = GetMax(biggestOfFirstTwo, thirdNumber);
+
+            Console.WriteLine(max);
+        }
+
+        private static int GetMax(int firstNumber, int secondNumber)
+        {
+            return firstNumber > secondNumber ? firstNumber : secondNumber;
+        }
+    }
+}
