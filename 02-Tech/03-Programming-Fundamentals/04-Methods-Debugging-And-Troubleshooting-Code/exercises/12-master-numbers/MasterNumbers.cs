1e89e8da609cb5db85d007ba327fe2f4a9521713
Tectonik <marto7395@gmail.com>
2018-05-01 23:54:30 +0300

    Finished "Magic numbers"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/12-master-numbers/MasterNumbers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _12_master_numbers
+{
+    class MasterNumbers
+    {
+        static void Main()
+        {
+            var limit = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = 1; currentNumber <= limit; currentNumber++)
+            {
+                if (ContainsEvenDigit(currentNumber)
+                    && SumOfDigits(currentNumber) % 7 == 0
+                    && IsPalindrome(currentNumber))
+                {
+                    Console.WriteLine(currentNumber);
+                }
+            }
+        }
+
+        private static bool IsPalindrome(int number)
+        {
+            var numberString = number.ToString();
+            return numberString.SequenceEqual(numberString.Reverse());
+        }
+
+        private static int SumOfDigits(int number)
+        {
+            var sum = 0;
+            while (number > 0)
+            {
+                sum += (number % 10);
+                number /= 10;
+            }
+
+            return sum;
+        }
+
+        private static bool ContainsEvenDigit(int number)
+        {
+            while (number > 0)
+            {
+                if (number % 2 == 0)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
+        }
+    }
+}
