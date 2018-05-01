0dac7d5af94c628b5760056eef3b2b3082c87c13
Tectonik <marto7395@gmail.com>
2018-05-01 23:58:35 +0300

    Finished "Factorial"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/13-factorial/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _13_factorial
+{
+    class Factorial
+    {
+        static void Main()
+        {
+            var targetFactorial = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for (int current = 1; current <= targetFactorial; current++)
+            {
+                factorial *= current;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
