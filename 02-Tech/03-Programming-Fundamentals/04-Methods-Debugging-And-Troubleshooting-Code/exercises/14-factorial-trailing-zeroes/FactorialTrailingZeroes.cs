298e9086f9d54009df86613900b7d4422903c756
Tectonik <marto7395@gmail.com>
2018-05-02 00:06:51 +0300

    Finished "Factorial trailing zeroes"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/14-factorial-trailing-zeroes/FactorialTrailingZeroes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Numerics;
+
+namespace _14_factorial_trailing_zeroes
+{
+    class FactorialTrailingZeroes
+    {
+        static void Main()
+        {
+            var targetFactorial = int.Parse(Console.ReadLine());
+            var factorial = CalculateFactorial(targetFactorial);
+            var trailingZeroes = TrailingZeroes(factorial);
+            Console.WriteLine(trailingZeroes);
+        }
+
+        private static BigInteger CalculateFactorial(int target)
+        {
+            BigInteger factorial = 1;
+
+            for (int current = 1; current <= target; current++)
+            {
+                factorial *= current;
+            }
+
+            return factorial;
+        }
+
+        private static int TrailingZeroes(BigInteger number)
+        {
+            var numberOfZeroes = 0;
+
+            while (true)
+            {
+                if (number % 10 == 0)
+                {
+                    numberOfZeroes++;
+                    number /= 10;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return numberOfZeroes;
+        }
+    }
+}
