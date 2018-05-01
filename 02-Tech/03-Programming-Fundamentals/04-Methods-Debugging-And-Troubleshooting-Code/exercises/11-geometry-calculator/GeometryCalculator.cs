4bdbc7650cc29475061bd61c9ffec561c8310b6a
Tectonik <marto7395@gmail.com>
2018-05-01 23:24:56 +0300

    Finished "Geometry calculator"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/11-geometry-calculator/GeometryCalculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _11_geometry_calculator
+{
+    class GeometryCalculator
+    {
+        static void Main()
+        {
+            var figure = Console.ReadLine();
+            var area = 0.0;
+            // FIXME: Using explicit scopes. Better way?
+            switch (figure.ToLower())
+            {
+                case "triangle":
+                    {
+                        var side = double.Parse(Console.ReadLine());
+                        var height = double.Parse(Console.ReadLine());
+                        area = GetTriangleArea(side, height);
+                    }
+                    break;
+
+                case "square":
+                    {
+                        var side = double.Parse(Console.ReadLine());
+                        area = GetRectangleArea(side, side);
+                    }
+                    break;
+
+                case "rectangle":
+                    {
+                        var width = double.Parse(Console.ReadLine());
+                        var height = double.Parse(Console.ReadLine());
+                        area = GetRectangleArea(width, height);
+                    }
+                    break;
+
+                case "circle":
+                    {
+                        var radius = double.Parse(Console.ReadLine());
+                        area = GetCircleArea(radius);
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"{area:0.00}");
+        }
+
+        private static double GetCircleArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+
+        private static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+
+        private static double GetTriangleArea(double side, double height)
+        {
+            return (side * height) / 2;
+        }
+    }
+}
