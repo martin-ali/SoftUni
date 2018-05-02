c3470527d77aa84c82b9c58de4ff81dda42c58c4
Tectonik <marto7395@gmail.com>
2018-05-02 22:28:47 +0300

    Refactored "LongerLine". Horribly over-complicated the solution.

--- 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/09-longer-line/LongerLine.cs
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/09-longer-line/LongerLine.cs
@@ -4,6 +4,7 @@ namespace _09_longer_line
 {
     class LongerLine
     {
+        // FIXME: Refactor the whole thing!
         static void Main()
         {
             // First Line
@@ -18,46 +19,77 @@ namespace _09_longer_line
             var x4 = double.Parse(Console.ReadLine());
             var y4 = double.Parse(Console.ReadLine());
 
-            var firstPoint = (x: x1, y: y1);
-            var secondPoint = (x: x2, y: y2);
-            var thirdPoint = (x: x3, y: y3);
-            var fourthPoint = (x: x4, y: y4);
+            var firstLineStart = new PointD(x1, y1);
+            var firstLineEnd = new PointD(x2, y2);
+            var secondLineStart = new PointD(x3, y3);
+            var secondLineEnd = new PointD(x4, y4);
 
-            var longestLine = GetLongestLine(firstPoint, secondPoint, thirdPoint, fourthPoint);
-            Console.WriteLine($"({longestLine.start.x}, {longestLine.start.y})({longestLine.end.x}, {longestLine.end.y})");
+            var firstLine = new Line(start: firstLineStart, end: firstLineEnd);
+            var secondLine = new Line(start: secondLineStart, end: secondLineEnd);
+
+            var longestLine = GetLongestLine(firstLine, secondLine);
+            longestLine = SortLineByDistanceToPoint(longestLine, new PointD(0, 0));
+            Console.WriteLine($"({longestLine.Start.X}, {longestLine.Start.Y})({longestLine.End.X}, {longestLine.End.Y})");
         }
 
-        private static ((double x, double y) start, (double x, double y) end) GetLongestLine((double x, double y) firstLineStart, (double x, double y) firstLineEnd, (double x, double y) secondLineStart, (double x, double y) secondLineEnd)
+        // TODO: Needs a better name
+        private static Line SortLineByDistanceToPoint(Line line, PointD comparisonPoint)
         {
-            var secondPointIsCloserThanFirst = DistanceBetweenPoints(firstLineEnd, (0, 0)) < DistanceBetweenPoints(firstLineStart, (0, 0));
-            if (secondPointIsCloserThanFirst)
+            var start = DistanceBetweenPoints(line.Start, comparisonPoint);
+            var end = DistanceBetweenPoints(line.End, comparisonPoint);
+
+            bool endIsCloserToComparisonPointThanStart = (end < start);
+            if (endIsCloserToComparisonPointThanStart)
             {
-                (firstLineStart, firstLineEnd) = (firstLineEnd, firstLineStart);
+                return new Line(line.End, line.Start);
             }
 
-            var fourthPointIsCloserThanThird = DistanceBetweenPoints(secondLineEnd, (0, 0)) < DistanceBetweenPoints(secondLineStart, (0, 0));
-            if (fourthPointIsCloserThanThird)
-            {
-                (secondLineStart, secondLineEnd) = (secondLineEnd, secondLineStart);
-            }
-
-            var firstLine = (start: firstLineStart, end: firstLineEnd);
-            var secondLine = (start: secondLineStart, end: secondLineEnd);
-
-            var firstLineLength = DistanceBetweenPoints(firstLineStart, firstLineEnd);
-            var secondLineLength = DistanceBetweenPoints(secondLineStart, secondLineEnd);
-
-            var longestLine = (firstLineLength >= secondLineLength ? firstLine : secondLine);
-            return longestLine;
+            return line;
         }
 
-        private static double DistanceBetweenPoints((double x, double y) firstPoint, (double x, double y) secondPoint)
+        private static Line GetLongestLine(Line firstLine, Line secondLine)
         {
-            var horizontalDistance = Math.Pow(firstPoint.x - secondPoint.x, 2);
-            var verticalDistance = Math.Pow(firstPoint.y - secondPoint.y, 2);
+            var firstLineLength = DistanceBetweenPoints(firstLine.Start, firstLine.End);
+            var secondLineLength = DistanceBetweenPoints(secondLine.Start, secondLine.End);
+
+            return firstLineLength >= secondLineLength ? firstLine : secondLine;
+        }
+
+        private static double DistanceBetweenPoints(PointD firstPoint, PointD secondPoint)
+        {
+            var horizontalDistance = Math.Pow(firstPoint.X - secondPoint.X, 2);
+            var verticalDistance = Math.Pow(firstPoint.Y - secondPoint.Y, 2);
             var distance = Math.Sqrt(horizontalDistance + verticalDistance);
 
             return distance;
         }
+    }
+
+    internal class Line
+    {
+
+        public Line(PointD start, PointD end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public PointD Start { get; set; }
+
+        public PointD End { get; set; }
+    }
+
+    internal class PointD
+    {
+
+        public PointD(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
     }
 }
