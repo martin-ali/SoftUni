2eb7ede29a56e43e7d01f97f2874d8387a500e3c
Tectonik <marto7395@gmail.com>
2018-05-03 22:15:52 +0300

    Re-named class PointD to Point

--- 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/09-longer-line/LongerLine.cs
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/09-longer-line/LongerLine.cs
@@ -18,21 +18,21 @@ namespace _09_longer_line
             var x4 = double.Parse(Console.ReadLine());
             var y4 = double.Parse(Console.ReadLine());
 
-            var firstLineStart = new PointD(x1, y1);
-            var firstLineEnd = new PointD(x2, y2);
-            var secondLineStart = new PointD(x3, y3);
-            var secondLineEnd = new PointD(x4, y4);
+            var firstLineStart = new Point(x1, y1);
+            var firstLineEnd = new Point(x2, y2);
+            var secondLineStart = new Point(x3, y3);
+            var secondLineEnd = new Point(x4, y4);
 
             var firstLine = new Line(start: firstLineStart, end: firstLineEnd);
             var secondLine = new Line(start: secondLineStart, end: secondLineEnd);
 
             var longestLine = GetLongestLine(firstLine, secondLine);
-            longestLine = SortLineByDistanceToPoint(longestLine, new PointD(0, 0));
+            longestLine = SortLineByDistanceToPoint(longestLine, new Point(0, 0));
             Console.WriteLine($"({longestLine.Start.X}, {longestLine.Start.Y})({longestLine.End.X}, {longestLine.End.Y})");
         }
 
         // TODO: Needs a better name
-        private static Line SortLineByDistanceToPoint(Line line, PointD comparisonPoint)
+        private static Line SortLineByDistanceToPoint(Line line, Point comparisonPoint)
         {
             var start = DistanceBetweenPoints(line.Start, comparisonPoint);
             var end = DistanceBetweenPoints(line.End, comparisonPoint);
@@ -51,10 +51,10 @@ namespace _09_longer_line
             var firstLineLength = DistanceBetweenPoints(firstLine.Start, firstLine.End);
             var secondLineLength = DistanceBetweenPoints(secondLine.Start, secondLine.End);
 
-            return firstLineLength >= secondLineLength ? firstLine : secondLine;
+            return (firstLineLength >= secondLineLength ? firstLine : secondLine);
         }
 
-        private static double DistanceBetweenPoints(PointD firstPoint, PointD secondPoint)
+        private static double DistanceBetweenPoints(Point firstPoint, Point secondPoint)
         {
             var horizontalDistance = Math.Pow(firstPoint.X - secondPoint.X, 2);
             var verticalDistance = Math.Pow(firstPoint.Y - secondPoint.Y, 2);
@@ -67,21 +67,21 @@ namespace _09_longer_line
     internal class Line
     {
 
-        public Line(PointD start, PointD end)
+        public Line(Point start, Point end)
         {
             this.Start = start;
             this.End = end;
         }
 
-        public PointD Start { get; set; }
+        public Point Start { get; set; }
 
-        public PointD End { get; set; }
+        public Point End { get; set; }
     }
 
-    internal class PointD
+    internal class Point
     {
 
-        public PointD(double x, double y)
+        public Point(double x, double y)
         {
             this.X = x;
             this.Y = y;
