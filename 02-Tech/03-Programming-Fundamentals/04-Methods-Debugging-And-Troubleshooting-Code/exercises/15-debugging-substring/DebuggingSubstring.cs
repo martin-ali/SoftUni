2138191c760021a0d5eca78f98fcc7390f3c2920
Tectonik <marto7395@gmail.com>
2018-05-03 22:14:38 +0300

    Finished "Debugging substring"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/15-debugging-substring/DebuggingSubstring.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _15_debugging_substring
+{
+    class DebuggingSubstring
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            const char Search = 'p';
+            bool hasMatch = false;
+
+            for (int index = 0; index < text.Length; index++)
+            {
+                var currentChar = text[index];
+                if (currentChar == Search)
+                {
+                    hasMatch = true;
+
+                    int length = count + 1;
+
+                    if ((length + index) > text.Length)
+                    {
+                        length = text.Length - index;
+                    }
+
+                    string matchedString = text.Substring(index, length);
+                    Console.WriteLine(matchedString);
+                    index += count;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
