122c7f341f0fd61f06be639288a0d05a71e221df
Tectonik <marto7395@gmail.com>
2018-05-04 00:00:57 +0300

    Finished "Fold and sum"

---
+++ 02-Tech/03-Programming-Fundamentals/05-Arrays/exercises/03-fold-and-sum/FoldAndSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_fold_and_sum
+{
+    class FoldAndSum
+    {
+        static void Main()
+        {
+            var elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var k = elements.Length / 4;
+
+            var leftWing = elements.Take(k).Reverse();
+            var rightWing = elements.Skip(k * 3).Reverse();
+            var center = elements.Skip(k).Take(k * 2).ToArray();
+
+            var firstRow = leftWing.Concat(rightWing).ToArray();
+            var secondRow = center;
+
+            // Simple method
+            // var sum = new int[firstRow.Length];
+            // for (int current = 0; current < firstRow.Length; current++)
+            // {
+            //     sum[current] = firstRow[current] + secondRow[current];
+            // }
+
+            // LINQ method
+            var sum = firstRow.Select((element, index) => element + secondRow[index]);
+
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
