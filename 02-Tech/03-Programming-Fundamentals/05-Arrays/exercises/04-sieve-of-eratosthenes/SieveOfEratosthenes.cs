564b348af258d55726b5c6d1fe6a30a6414dcce7
Tectonik <marto7395@gmail.com>
2018-05-04 00:35:38 +0300

    Finished "Sieve of Eratosthenes"

---
+++ 02-Tech/03-Programming-Fundamentals/05-Arrays/exercises/04-sieve-of-eratosthenes/SieveOfEratosthenes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_sieve_of_eratosthenes
+{
+    class SieveOfEratosthenes
+    {
+        static void Main()
+        {
+            var rangeEnd = int.Parse(Console.ReadLine());
+
+            var primes = new bool[rangeEnd + 1].Select(x => true).ToArray();
+            primes[0] = primes[1] = false;
+
+            for (int candidate = 0; candidate <= rangeEnd; candidate++)
+            {
+                if (primes[candidate])
+                {
+                    Console.Write(candidate + " ");
+                    for (int current = candidate + candidate; current <= rangeEnd; current += candidate)
+                    {
+                        primes[current] = false;
+                    }
+                }
+            }
+        }
+    }
+}
