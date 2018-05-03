8e53aabd3763929051be41c5845264c052701a38
Tectonik <marto7395@gmail.com>
2018-05-03 22:14:55 +0300

    Finished "debugging instruction set"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/exercises/16-debugging-instruction-set/DebuggingInstructionSet.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _16_debugging_instruction_set
+{
+    class DebuggingInstructionSet
+    {
+        public static void Main()
+        {
+
+            string opCode = Console.ReadLine();
+
+            while (opCode != "END")
+            {
+                string[] codeArgs = opCode.Split(' ');
+
+                long result = 0;
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = int.Parse(codeArgs[1]);
+                            result = ++operandOne;
+                            break;
+                        }
+                    case "DEC":
+                        {
+                            long operandOne = int.Parse(codeArgs[1]);
+                            result = --operandOne;
+                            break;
+                        }
+                    case "ADD":
+                        {
+                            int operandOne = int.Parse(codeArgs[1]);
+                            int operandTwo = int.Parse(codeArgs[2]);
+                            result = (long)operandOne + operandTwo;
+                            break;
+                        }
+                    case "MLA":
+                        {
+                            int operandOne = int.Parse(codeArgs[1]);
+                            int operandTwo = int.Parse(codeArgs[2]);
+                            result = (long)operandOne * operandTwo;
+                            break;
+                        }
+                }
+
+                Console.WriteLine(result);
+                opCode = Console.ReadLine();
+            }
+        }
+    }
+}
+/*
+INC 0
+ADD 1323134 421315521
+DEC 57314183
+MLA 252621 324532
+END
+ */
