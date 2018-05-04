0398a5bb861d560f3ef99e2083113327d4dc2cf0
Tectonik <marto7395@gmail.com>
2018-05-04 22:30:08 +0300

    Finished "Grab and go"

---
+++ 02-Tech/03-Programming-Fundamentals/05-Arrays/extra-exercises/04-grab-and-go/GrabAndGo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_grab_and_go
+{
+    class GrabAndGo
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+            var elementToFind = int.Parse(Console.ReadLine());
+
+            var indexOfElement = arr.LastIndexOf(elementToFind);
+
+            var elementIsFound = (indexOfElement >= 0);
+            if (elementIsFound)
+            {
+                var previousElementsSum = arr.Take(indexOfElement).Sum();
+                Console.WriteLine(previousElementsSum);
+            }
+            else
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+        }
+    }
+}
+/*
+
+10 20 30 40 20 30 40
+20
+
+1 3 5 7 12 2 3 5 12
+3
+
+1 2 3 4 5 6 7 8 9 10
+20
+
+ */
