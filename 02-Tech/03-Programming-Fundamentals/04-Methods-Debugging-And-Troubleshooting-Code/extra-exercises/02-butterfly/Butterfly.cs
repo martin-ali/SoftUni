92e4bd2a8643835eac1068651d445b30884a8f08
Tectonik <marto7395@gmail.com>
2018-05-04 17:21:49 +0300

    Added "Butterfly"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/extra-exercises/02-butterfly/Butterfly.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02_butterfly
+{
+    class Butterfly
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            int height = 2 * (size - 2) + 1;
+            int width = (2 * size) - 1;
+
+            // Draw top
+            int wingWidth = size - 2;
+            string wingStar = new String('*', wingWidth);
+            string wingDash = new String('-', wingWidth);
+            for (int row = 2; row < size; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    Console.WriteLine($"{wingStar}\\ /{wingStar}");
+                }
+                else
+                {
+                    Console.WriteLine($"{wingDash}\\ /{wingDash}");
+                }
+            }
+
+            // Draw mid
+            Console.WriteLine($"{new String(' ', size - 1)}@");
+
+            // Draw bottom
+            for (int row = 2; row < size; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    Console.WriteLine($"{wingStar}/ \\{wingStar}");
+                }
+                else
+                {
+                    Console.WriteLine($"{wingDash}/ \\{wingDash}");
+                }
+            }
+        }
+    }
+}
