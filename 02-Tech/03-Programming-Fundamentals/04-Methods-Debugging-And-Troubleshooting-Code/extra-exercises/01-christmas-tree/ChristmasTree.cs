e751a8f28cbe8297f41cb6e246922ebcc44d71ee
Tectonik <marto7395@gmail.com>
2018-05-04 17:21:34 +0300

    Added "Christmas tree"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/extra-exercises/01-christmas-tree/ChristmasTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01_christmas_tree
+{
+    class ChristmasTree
+    {
+        static void Main()
+        {
+            var size = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= size; i++)
+            {
+                var leaves = new string('*', i);
+                var padding = new string(' ', size - i);
+                Console.WriteLine($"{padding}{leaves} | {leaves}");
+            }
+        }
+    }
+}
