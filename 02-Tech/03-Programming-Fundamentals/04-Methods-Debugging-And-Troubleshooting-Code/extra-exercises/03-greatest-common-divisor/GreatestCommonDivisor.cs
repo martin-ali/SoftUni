fed44aa8443b36e05fc834ade6eb53913b6005b6
Tectonik <marto7395@gmail.com>
2018-05-04 17:25:57 +0300

    Added "Greatest common divisor"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/extra-exercises/03-greatest-common-divisor/GreatestCommonDivisor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _03_greatest_common_divisor
+{
+    class GreatestCommonDivisor
+    {
+        static void Main()
+        {
+            int numA = int.Parse(Console.ReadLine());
+            int numB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(findGreatestCommonDivisor(numA, numB));
+        }
+
+        static int findGreatestCommonDivisor(int a, int b)
+        {
+            // Find more elegant way
+            if (a == 0)
+            {
+                return b;
+            }
+            if (b == 0)
+            {
+                return a;
+            }
+
+            if (a > b)
+            {
+                int greatestCommonDivisor = findGreatestCommonDivisor(a % b, b);
+                return greatestCommonDivisor;
+            }
+            else
+            {
+                int greatestCommonDivisor = findGreatestCommonDivisor(a, b % a);
+                return greatestCommonDivisor;
+            }
+        }
+    }
+}
