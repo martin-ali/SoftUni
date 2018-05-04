5c8e8588ce3fb2230db87ff55b3fc145f72e3a52
Tectonik <marto7395@gmail.com>
2018-05-04 17:27:26 +0300

    Added "Number pyramid"

---
+++ 02-Tech/03-Programming-Fundamentals/04-Methods-Debugging-And-Troubleshooting-Code/extra-exercises/04-number-pyramid/NumberPyramid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_number_pyramid
+{
+    class NumberPyramid
+    {
+        static void Main()
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            for (int currentRowNumber = 1, currentNumber = 1; currentNumber <= num; currentRowNumber++)
+            {
+                for (int currentCol = 1; currentCol <= currentRowNumber && currentNumber <= num; currentCol++)
+                {
+                    Console.Write($"{currentNumber++} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
