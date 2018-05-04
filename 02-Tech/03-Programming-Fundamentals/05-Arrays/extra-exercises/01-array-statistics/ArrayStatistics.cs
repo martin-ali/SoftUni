95be942db138551b2ac511bc17dcce1764ec53e8
Tectonik <marto7395@gmail.com>
2018-05-04 17:43:21 +0300

    Finished "Array statistics"

---
+++ 02-Tech/03-Programming-Fundamentals/05-Arrays/extra-exercises/01-array-statistics/ArrayStatistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_array_statistics
+{
+    class ArrayStatistics
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console
+                        .ReadLine()
+                        .Split(' ')
+                        .Select(int.Parse)
+                        .ToArray();
+
+            var min = arr.Min();
+            var max = arr.Max();
+            var sum = arr.Sum();
+            var average = arr.Average();
+
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average}");
+        }
+    }
+}
