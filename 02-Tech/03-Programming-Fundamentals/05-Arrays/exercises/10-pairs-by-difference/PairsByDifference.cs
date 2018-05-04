3336ea83e3c38c6843b188168e40e90bbfcfe8ef
Tectonik <marto7395@gmail.com>
2018-05-04 16:07:02 +0300

    Finished "Pairs by difference"

---
+++ 02-Tech/03-Programming-Fundamentals/05-Arrays/exercises/10-pairs-by-difference/PairsByDifference.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _10_pairs_by_difference
+{
+    class PairsByDifference
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var difference = int.Parse(Console.ReadLine());
+
+            var numberOfPairs = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int current = numbers[i];
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    var currentPairEqualsSum = (current - numbers[j] == difference) || (current - numbers[j] == -difference);
+                    if (currentPairEqualsSum)
+                    {
+                        numberOfPairs++;
+                    }
+                }
+            }
+
+            Console.WriteLine(numberOfPairs);
+        }
+    }
+}
+/*
+
+1 5 3 4 2
+2
+
+5 3 8 10 12 1
+1
+
+ */
