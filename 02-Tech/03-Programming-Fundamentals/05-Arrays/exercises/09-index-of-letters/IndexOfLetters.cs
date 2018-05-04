00f3b00ebcb38b467c47340f52b55857d651778f
Tectonik <marto7395@gmail.com>
2018-05-04 15:06:39 +0300

    Finished "Index of letters"

---
+++ 02-Tech/03-Programming-Fundamentals/05-Arrays/exercises/09-index-of-letters/IndexOfLetters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09_index_of_letters
+{
+    class IndexOfLetters
+    {
+        static void Main()
+        {
+            var word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                var character = word[i];
+                var characterCode = (int)character - 97;
+                Console.WriteLine($"{character} -> {characterCode}");
+            }
+        }
+    }
+}
