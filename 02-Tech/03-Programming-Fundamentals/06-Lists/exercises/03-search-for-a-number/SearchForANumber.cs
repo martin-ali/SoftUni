c211b8eb6aabdcdcd78ff59adf14859ddc5367a3
Tectonik <marto7395@gmail.com>
2018-05-05 23:58:12 +0300

    Finished "Search for a number"

---
+++ 02-Tech/03-Programming-Fundamentals/06-Lists/exercises/03-search-for-a-number/SearchForANumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03_search_for_a_number
+{
+    class SearchForANumber
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse);
+            var arguments = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var numberFound = numbers
+                            .Take(arguments[0])
+                            .Skip(arguments[1])
+                            .Contains(arguments[2]);
+
+            Console.WriteLine(numberFound ? "YES!" : "NO!");
+        }
+    }
+}
