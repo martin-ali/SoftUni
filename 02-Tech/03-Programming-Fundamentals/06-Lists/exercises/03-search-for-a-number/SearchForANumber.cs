d88e444309aabe83b87122ea50d79df36ced7cfd
Tectonik <marto7395@gmail.com>
2018-05-06 10:13:08 +0300

    Small formatting fix

--- 02-Tech/03-Programming-Fundamentals/06-Lists/exercises/03-search-for-a-number/SearchForANumber.cs
+++ 02-Tech/03-Programming-Fundamentals/06-Lists/exercises/03-search-for-a-number/SearchForANumber.cs
@@ -11,9 +11,9 @@ namespace _03_search_for_a_number
             var arguments = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
             var numberFound = numbers
-                            .Take(arguments[0])
-                            .Skip(arguments[1])
-                            .Contains(arguments[2]);
+                                .Take(arguments[0])
+                                .Skip(arguments[1])
+                                .Contains(arguments[2]);
 
             Console.WriteLine(numberFound ? "YES!" : "NO!");
         }
