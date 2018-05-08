95111efe510efa45606eb2983375a45b03bf720f
Tectonik <marto7395@gmail.com>
2018-05-08 21:12:34 +0300

    Finished "Bomb numbers"

---
+++ 02-Tech/03-Programming-Fundamentals/06-Lists/exercises/07-bomb-numbers/BombNumbers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _07_bomb_numbers
+{
+    class BombNumbers
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').ToList();
+            var arguments = Console.ReadLine().Split(' ').ToList();
+            var bomb = arguments[0];
+            var power = int.Parse(arguments[1]);
+
+            for (int index = 0; index < numbers.Count; index++)
+            {
+                if (numbers[index] == bomb)
+                {
+                    var start = Math.Max(index - power, 0);
+                    var end = Math.Min(index + power, numbers.Count - 1);
+
+                    for (int current = start; current <= end; current++)
+                    {
+                        numbers[current] = null;
+                    }
+                }
+            }
+
+            var sum = numbers.Where(x => x != null).Select(int.Parse).Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
+/*
+
+1 2 2 4 2 2 2 9
+4 2
+
+1 4 4 2 8 9 1
+9 3
+
+1 7 7 1 2 3
+7 1
+
+1 1 2 1 1 1 2 1 1 1
+2 1
+
+ */
