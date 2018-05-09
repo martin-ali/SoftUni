8039390c9d5aca389374adfa3ca43ee73618e8a2
Tectonik <marto7395@gmail.com>
2018-05-09 22:51:57 +0300

    Finished "Fix emails"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/exercises/04-fix-emails/FixEmails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace _04_fix_emails
+{
+    class FixEmails
+    {
+        static void Main()
+        {
+            var command = Console.ReadLine();
+            while (command != "stop")
+            {
+                var name = command;
+                var email = Console.ReadLine();
+
+                string emailLowercase = email.ToLower();
+                if (emailLowercase.EndsWith("us") == false
+                    && emailLowercase.EndsWith("uk") == false)
+                {
+                    Console.WriteLine($"{name} -> {email}");
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
+/*
+
+Ivan
+ivanivan@abv.bg
+Petar Ivanov
+petartudjarov@abv.bg
+Mike Tyson
+myke@gmail.us
+stop
+
+ */
