932a34b2d84a2d76a22867a594ff4d526297bf7d
Tectonik <marto7395@gmail.com>
2018-05-09 22:42:08 +0300

    Finished "A miner task"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/exercises/03-a-miner-task/AMinerTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_a_miner_task
+{
+    class AMinerTask
+    {
+        static void Main()
+        {
+            var storage = new Dictionary<string, int>();
+
+            var line = Console.ReadLine();
+            while (line.Equals("stop") == false)
+            {
+
+                var resource = line;
+                var quantity = int.Parse(Console.ReadLine());
+
+                if (storage.ContainsKey(resource) == false)
+                {
+                    storage[resource] = 0;
+                }
+
+                storage[resource] += quantity;
+                line = Console.ReadLine();
+            }
+
+            foreach (var resource in storage)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
