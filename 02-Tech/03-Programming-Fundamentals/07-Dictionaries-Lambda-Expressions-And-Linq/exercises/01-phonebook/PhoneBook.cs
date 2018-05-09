3714ea3895c21f7e9e2908f580993150fd3a56ec
Tectonik <marto7395@gmail.com>
2018-05-09 22:25:55 +0300

    Finished "Phonebook"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/exercises/01-phonebook/PhoneBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_phonebook
+{
+    class PhoneBook
+    {
+        static void Main()
+        {
+            var phonebook = new Dictionary<string, string>();
+            var command = Console.ReadLine().Split(' ');
+            while (command[0] != ("END"))
+            {
+                if (command[0] == "A")
+                {
+                    var name = command[1];
+                    var number = command[2];
+                    phonebook[name] = number;
+                }
+                else if (command[0] == "S")
+                {
+                    var name = command[1];
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+
+                command = Console.ReadLine().Split(' ');
+            }
+        }
+    }
+}
