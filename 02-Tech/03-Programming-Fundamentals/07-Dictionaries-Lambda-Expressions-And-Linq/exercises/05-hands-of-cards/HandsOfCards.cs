a0036a24d769981d12b59e6cb21fcc20cf39c809
Tectonik <marto7395@gmail.com>
2018-05-10 10:13:55 +0300

    Finished "Hands of cards"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/exercises/05-hands-of-cards/HandsOfCards.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_hands_of_cards
+{
+    class HandsOfCards
+    {
+        private static Dictionary<string, int> cardPowersBySuite = new Dictionary<string, int>
+        {
+            ["2"] = 2,
+            ["3"] = 3,
+            ["4"] = 4,
+            ["5"] = 5,
+            ["6"] = 6,
+            ["7"] = 7,
+            ["8"] = 8,
+            ["9"] = 9,
+            ["10"] = 10,
+            ["J"] = 11,
+            ["Q"] = 12,
+            ["K"] = 13,
+            ["A"] = 14
+        };
+
+        private static Dictionary<char, int> cardTypesByFace = new Dictionary<char, int>
+        {
+            ['S'] = 4,
+            ['H'] = 3,
+            ['D'] = 2,
+            ['C'] = 1
+        };
+
+        static void Main()
+        {
+            var playersAndTheirHands = new Dictionary<string, HashSet<string>>();
+
+            var command = Console.ReadLine().Split(new string[] { ", ", ": " }, StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "JOKER")
+            {
+                var name = command[0];
+                var cards = command.Skip(1);
+                var hand = new HashSet<string>(cards);
+
+                if (playersAndTheirHands.ContainsKey(name) == false)
+                {
+                    playersAndTheirHands[name] = new HashSet<string>();
+                }
+
+                playersAndTheirHands[name].UnionWith(hand);
+
+                command = Console.ReadLine().Split(new string[] { ", ", ": " }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var player in playersAndTheirHands)
+            {
+                Console.WriteLine($"{player.Key}: {player.Value.Select(card => GetCardPower(card)).Sum()}");
+            }
+        }
+
+        private static int GetCardPower(string card)
+        {
+            string suite = card.Substring(0, card.Length - 1);
+            char face = card.Last();
+
+            var power = cardPowersBySuite[suite] * cardTypesByFace[face];
+            return power;
+        }
+    }
+}
+/*
+
+Pesho: 2C, 4H, 9H, AS, QS
+Slav: 3H, 10S, JC, KD, 5S, 10S
+Peshoslav: QH, QC, QS, QD
+Slav: 6H, 7S, KC, KD, 5S, 10C
+Peshoslav: QH, QC, JS, JD, JC
+Pesho: JD, JD, JD, JD, JD, JD
+JOKER
+
+ */
