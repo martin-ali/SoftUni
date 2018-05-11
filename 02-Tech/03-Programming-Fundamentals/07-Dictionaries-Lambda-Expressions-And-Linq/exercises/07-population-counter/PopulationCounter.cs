bc6e76c4d64bb647e0ea3de40b82c823cb76c581
Tectonik <marto7395@gmail.com>
2018-05-11 21:19:57 +0300

    Finished "Population counter"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/exercises/07-population-counter/PopulationCounter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _07_population_counter
+{
+    class PopulationCounter
+    {
+        static void Main()
+        {
+            #if DEBUG
+            Console.SetIn(new StreamReader("tests/test2.txt"));
+            #endif
+
+            var countriesCitiesAndPopulations = new Dictionary<string, SortedDictionary<string, long>>();
+            var countriesAndTheirPopulation = new Dictionary<string, long>();
+
+            var command = Console.ReadLine().Split('|');
+            while (command[0] != "report")
+            {
+                var city = command[0];
+                var country = command[1];
+                var population = int.Parse(command[2]);
+
+                if (countriesCitiesAndPopulations.ContainsKey(country) == false)
+                {
+                    countriesCitiesAndPopulations[country] = new SortedDictionary<string, long>();
+                }
+
+                if (countriesAndTheirPopulation.ContainsKey(country) == false)
+                {
+                    countriesAndTheirPopulation[country] = 0;
+                }
+
+                countriesCitiesAndPopulations[country][city] = population;
+                countriesAndTheirPopulation[country] += population;
+
+                command = Console.ReadLine().Split('|');
+            }
+
+            var sortedCountries = countriesAndTheirPopulation.OrderByDescending(country => country.Value);
+            foreach (var country in sortedCountries)
+            {
+                Console.WriteLine($"{country.Key} (total population: {countriesAndTheirPopulation[country.Key]})");
+
+                var sortedCities = countriesCitiesAndPopulations[country.Key].OrderByDescending(city => city.Value);
+                foreach (var city in sortedCities)
+                {
+                    Console.WriteLine($"=>{city.Key}: {city.Value}");
+                }
+            }
+        }
+    }
+}
