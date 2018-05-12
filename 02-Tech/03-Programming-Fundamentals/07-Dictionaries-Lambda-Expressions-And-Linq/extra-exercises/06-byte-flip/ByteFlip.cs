dfb1010c725f357a4ea795c2659e129eb156ee49
Tectonik <marto7395@gmail.com>
2018-05-12 22:56:11 +0300

    Finished "Byte flip"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/extra-exercises/06-byte-flip/ByteFlip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _06_byte_flip
+{
+    class ByteFlip
+    {
+        static void Main()
+        {
+
+            #if DEBUG
+            var testNumber = Console.ReadLine();
+            Console.SetIn(new StreamReader($"tests/test{testNumber}.txt"));
+            #endif
+
+            var message = Console
+                        .ReadLine()
+                        .Split(' ')
+                        .Where(x => x.Length == 2)
+                        .Select(x => $"{x[1]}{x[0]}")
+                        .Reverse()
+                        .Select(x => Convert.ToInt32(x, 16))
+                        .Select(x => (char)x);
+
+            Console.WriteLine(string.Join("", message));
+        }
+    }
+}
