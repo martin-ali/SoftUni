7506f2e05a8b3b4d44d5791a73ebb910b77e282c
Tectonik <marto7395@gmail.com>
2018-05-12 15:18:19 +0300

    Finished "Sort times"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/extra-exercises/01-sort-times/SortTimes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_sort_times
+{
+    class SortTimes
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').OrderBy(time => time);
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
+
+/*
+
+00:00 06:04 02:59 10:33 11:22 06:01
+04:25 04:21 04:19
+00:00 23:59 12:00 16:00
+
+ */
