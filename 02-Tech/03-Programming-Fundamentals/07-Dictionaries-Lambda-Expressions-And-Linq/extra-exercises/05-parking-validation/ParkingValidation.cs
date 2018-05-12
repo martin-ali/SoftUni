ab87f140696bf883a2e6b3a300808e0216e6a697
Tectonik <marto7395@gmail.com>
2018-05-12 22:44:59 +0300

    Finished "Parking validation"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/extra-exercises/05-parking-validation/ParkingValidation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _05_parking_validation
+{
+    class ParkingValidation
+    {
+
+        private static string licensePlatePattern = @"^[A-Z]{2}\d{4}[A-Z]{2}$";
+
+        static void Main()
+        {
+
+            #if DEBUG
+            var testNumber = Console.ReadLine();
+            Console.SetIn(new StreamReader($"tests/test{testNumber}.txt"));
+            #endif
+
+            var licensePlatesByUser = new Dictionary<string, string>();
+            var usedLicensePlates = new HashSet<string>();
+            var numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+
+                if (input[0] == "register")
+                {
+                    var user = input[1];
+                    var licensePlate = input[2];
+
+                    if (licensePlatesByUser.ContainsKey(user))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlatesByUser[user]}");
+                        continue;
+                    }
+                    else if (!IsValidLicensePlate(licensePlate))
+                    {
+                        Console.WriteLine($"ERROR: invalid license plate {licensePlate}");
+                        continue;
+                    }
+                    else if (usedLicensePlates.Contains(licensePlate))
+                    {
+                        Console.WriteLine($"ERROR: license plate {licensePlate} is busy");
+                        continue;
+                    }
+
+                    usedLicensePlates.Add(licensePlate);
+                    licensePlatesByUser[user] = licensePlate;
+                    Console.WriteLine($"{user} registered {licensePlate} successfully");
+                }
+                else
+                {
+                    var user = input[1];
+                    if (licensePlatesByUser.ContainsKey(user))
+                    {
+                        var licensePlate = licensePlatesByUser[user];
+                        usedLicensePlates.Remove(licensePlate);
+                        licensePlatesByUser.Remove(user);
+                        Console.WriteLine($"user {user} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {user} not found");
+                    }
+                }
+            }
+
+            foreach (var user in licensePlatesByUser)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+
+        private static bool IsValidLicensePlate(string licensePlate)
+        {
+            return Regex.IsMatch(licensePlate, licensePlatePattern) && licensePlate.Length == 8;
+        }
+    }
+}
