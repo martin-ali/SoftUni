507cdafe9087c686b6cc33910d72f6d0dbb4421e
Tectonik <marto7395@gmail.com>
2018-05-13 23:23:09 +0300

    Finished "Take/skip rope"

---
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/extra-exercises/07-take-skip-rope/TakeSkipRope.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _07_take_skip_rope
+{
+    class TakeSkipRope
+    {
+        static void Main()
+        {
+
+            #if DEBUG
+            var testNumber = Console.ReadLine();
+            Console.SetIn(new StreamReader($"tests/test{testNumber}.txt"));
+            #endif
+
+            var encryptedMessage = Console.ReadLine();
+            var numbers = encryptedMessage.Where(x => Char.IsDigit(x)).Select(x => (int)x - 48);
+            var notNumbers = encryptedMessage.Where(x => !Char.IsDigit(x));
+
+            var takeList = numbers.Where((x, i) => i % 2 == 0).ToList();
+            var skipList = numbers.Where((x, i) => i % 2 != 0).ToList();
+
+            var decryptedMessage = new List<char>();
+
+            var skipCount = 0;
+            for (int i = 0; i < takeList.Count; i++)
+            {
+                decryptedMessage.AddRange(notNumbers.Skip(skipCount).Take(takeList[i]));
+                skipCount += skipList[i] + takeList[i];
+            }
+
+            Console.WriteLine(string.Join("", decryptedMessage));
+        }
+    }
+}
