7b82456ec281688d9cddd9eacf3ac6767048332b
Tectonik <marto7395@gmail.com>
2018-05-14 23:09:51 +0300

    Improvements to code quality

--- 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/extra-exercises/07-take-skip-rope/TakeSkipRope.cs
+++ 02-Tech/03-Programming-Fundamentals/07-Dictionaries-Lambda-Expressions-And-Linq/extra-exercises/07-take-skip-rope/TakeSkipRope.cs
@@ -9,7 +9,6 @@ namespace _07_take_skip_rope
     {
         static void Main()
         {
-
             #if DEBUG
             var testNumber = Console.ReadLine();
             Console.SetIn(new StreamReader($"tests/test{testNumber}.txt"));
@@ -20,15 +19,14 @@ namespace _07_take_skip_rope
             var notNumbers = encryptedMessage.Where(x => !Char.IsDigit(x));
 
             var takeList = numbers.Where((x, i) => i % 2 == 0).ToList();
-            var skipList = numbers.Where((x, i) => i % 2 != 0).ToList();
-
-            var decryptedMessage = new List<char>();
+            var skipList = numbers.Where((x, i) => i % 2 == 1).ToList();
 
             var skipCount = 0;
+            var decryptedMessage = new List<char>();
             for (int i = 0; i < takeList.Count; i++)
             {
                 decryptedMessage.AddRange(notNumbers.Skip(skipCount).Take(takeList[i]));
-                skipCount += skipList[i] + takeList[i];
+                skipCount += (skipList[i] + takeList[i]);
             }
 
             Console.WriteLine(string.Join("", decryptedMessage));
