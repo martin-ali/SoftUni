bb3dbcf0ea5b4b90dc74dfd98598bd570839f384
Tectonik <marto7395@gmail.com>
2018-05-16 12:28:31 +0300

    Finished "Advertisement message"

---
+++ 02-Tech/03-Programming-Fundamentals/08-Objects-And-Classes/exercises/02-advertisement-message/AdvertisementMessage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _02_advertisement_message
+{
+    class AdvertisementMessage
+    {
+        private static string[] phrases = new string[]
+        {
+            "Excellent product.",
+            "Such a great product.",
+            "I always use that product.",
+            "Best product of its category.",
+            "Exceptional product.",
+            "I can’t live without this product."
+        };
+
+        private static string[] events = new string[]
+        {
+            "Now I feel good.",
+            "I have succeeded with this product.",
+            "Makes miracles. I am happy of the results!",
+            "I cannot believe but now I feel awesome.",
+            "Try it yourself, I am very satisfied.",
+            "I feel great!"
+        };
+
+        private static string[] authors = new string[]
+        {
+            "Diana",
+            "Petya",
+            "Stella",
+            "Elena",
+            "Katya",
+            "Iva",
+            "Annie",
+            "Eva"
+        };
+
+        private static string[] cities = new string[]
+        {
+            "Burgas",
+            "Sofia",
+            "Plovdiv",
+            "Varna",
+            "Ruse"
+        };
+
+        static void Main()
+        {
+            var random = new Random();
+            var numberOfMessages = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfMessages; i++)
+            {
+                var phraseIndex = random.Next(0, phrases.Length);
+                var eventIndex = random.Next(0, events.Length);
+                var authorIndex = random.Next(0, authors.Length);
+                var cityIndex = random.Next(0, cities.Length);
+
+                Console.WriteLine($"{phrases[phraseIndex]} {events[eventIndex]} {authors[authorIndex]} – {cities[cityIndex]}.");
+            }
+        }
+    }
+}
