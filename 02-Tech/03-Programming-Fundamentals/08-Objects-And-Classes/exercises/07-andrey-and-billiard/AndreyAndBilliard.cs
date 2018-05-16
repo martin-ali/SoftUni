ed0fd3e2ba9616dabe76e77be60466d3d7a1efb1
Tectonik <marto7395@gmail.com>
2018-05-16 17:48:51 +0300

    Finished "Andrey and billiard"

---
+++ 02-Tech/03-Programming-Fundamentals/08-Objects-And-Classes/exercises/07-andrey-and-billiard/AndreyAndBilliard.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _07_andrey_and_billiard
+{
+    class AndreyAndBilliard
+    {
+        static void Main()
+        {
+            #if DEBUG
+            Console.SetIn(new StreamReader("test.txt"));
+            #endif
+
+            var numberOfProducts = int.Parse(Console.ReadLine());
+            var pricesByProduct = new Dictionary<string, decimal>();
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                var parameters = Console.ReadLine().Split('-');
+                var product = parameters[0];
+                var price = decimal.Parse(parameters[1]);
+
+                pricesByProduct[product] = price;
+            }
+
+            char[] separators = new char[] { ',', '-' };
+            var input = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            var customersByName = new SortedDictionary<string, Customer>();
+            while (input[0] != "end of clients")
+            {
+                var customer = input[0];
+                var product = input[1];
+                var quantity = int.Parse(input[2]);
+
+                if (pricesByProduct.ContainsKey(product))
+                {
+                    if (customersByName.ContainsKey(customer) == false)
+                    {
+                        customersByName[customer] = new Customer(customer);
+                    }
+
+                    customersByName[customer].Order(product, quantity, pricesByProduct[product]);
+                }
+
+                input = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            var totalBill = 0m;
+            foreach (var customer in customersByName)
+            {
+                Console.WriteLine($"{customer.Key}");
+
+                foreach (var product in customer.Value.Orders)
+                {
+                    Console.WriteLine($"-- {product.Key} - {product.Value}");
+                }
+
+                Console.WriteLine($"Bill: {customer.Value.Bill:0.00}");
+                totalBill += customer.Value.Bill;
+            }
+
+            Console.WriteLine($"Total bill: {totalBill:0.00}");
+        }
+    }
+
+    internal class Customer
+    {
+        public Customer(string name)
+        {
+            this.Name = name;
+            this.Orders = new Dictionary<string, int>();
+        }
+
+        public decimal Bill { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Dictionary<string, int> Orders { get; set; }
+
+        public void Order(string product, int quantity, decimal price)
+        {
+            if (this.Orders.ContainsKey(product) == false)
+            {
+                this.Orders[product] = 0;
+            }
+
+            this.Orders[product] += quantity;
+            this.Bill += price * quantity;
+        }
+    }
+}
