ac5a2310e5028ae9f8947f762893cfce65def90c
Tectonik <marto7395@gmail.com>
2018-05-21 22:54:37 +0300

    Finished "Magic exchangeable words"

---
+++ 02-Tech/03-Programming-Fundamentals/09-Strings-And-Text-Processing/exercises/05-magic-exchangeable-words/MagicExchangeableWords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _05_magic_exchangeable_words
+{
+    class MagicExchangeableWords
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var first = input[0];
+            var second = input[1];
+
+            var wordsAreExchangeable = first.Distinct().Count() == second.Distinct().Count();
+            Console.WriteLine(wordsAreExchangeable.ToString().ToLower());
+        }
+    }
+}
