fb5969f92d8dcbb362655eb554c6913f4c52d23b
Tectonik <marto7395@gmail.com>
2018-05-21 22:16:13 +0300

    Finished "Finished "Convert from base N to base 10"

---
+++ 02-Tech/03-Programming-Fundamentals/09-Strings-And-Text-Processing/exercises/02-convert-from-base-n-to-base-10/ConvertFromBaseNToBase10.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02_convert_from_base_n_to_base_10
+{
+    class ConvertFromBaseNToBase10
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var number = input[1];
+            var fromBase = int.Parse(input[0]);
+
+            var nBaseNumber = ConvertBaseNToDecimal(number, fromBase);
+
+            Console.WriteLine(nBaseNumber);
+        }
+
+        private static BigInteger ConvertBaseNToDecimal(string number, int fromBase)
+        {
+            BigInteger Base10Number = 0;
+            var power = number.Length - 1;
+            for (int i = 0; i < number.Length; i++)
+            {
+                int digit = ((int)number[i] - 48);
+                BigInteger current = digit * BigInteger.Pow(fromBase, power--);
+                Base10Number += current;
+            }
+
+            return Base10Number;
+        }
+    }
+}
+
+/*
+
+7 13
+3 12201
+5 443
+4 33220
+9 4704
+2 1011
+
+ */
