b3bafe9e927414d2105e026f4dced0959fd83bb4
Tectonik <marto7395@gmail.com>
2018-05-21 14:40:10 +0300

    Finished "Convert from base 10 to base N"

---
+++ 02-Tech/03-Programming-Fundamentals/09-Strings-And-Text-Processing/exercises/01-convert-from-base-10-to-base-n/ConvertFromBase10ToBaseN.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace _01_convert_from_base_10_to_base_n
+{
+    class ConvertFromBase10ToBaseN
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(BigInteger.Parse).ToArray();
+            var toBase = input[0];
+            var decimalNumber = input[1];
+
+            var nBaseNumber = ConvertToBinary(decimalNumber, toBase);
+
+            Console.WriteLine(nBaseNumber);
+        }
+
+        private static string ConvertToBinary(BigInteger decimalNumber, BigInteger toBase)
+        {
+            var binaryNumber = "";
+            while (decimalNumber > 0)
+            {
+                var remainder = decimalNumber % toBase;
+                decimalNumber /= toBase;
+                binaryNumber = remainder + binaryNumber;
+            }
+
+            return binaryNumber;
+        }
+    }
+}
