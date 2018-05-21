14f4ae1347fcec5c8b7d190811345841f7e37f01
Tectonik <marto7395@gmail.com>
2018-05-21 22:26:23 +0300

    Finished "Unicode characters"

---
+++ 02-Tech/03-Programming-Fundamentals/09-Strings-And-Text-Processing/exercises/03-unicode-characters/UnicodeCharacters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03_unicode_characters
+{
+    class UnicodeCharacters
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write(GetUnicodeRepresentation(input[i]).ToLower());
+            }
+            Console.WriteLine();
+        }
+
+        static string GetUnicodeRepresentation(char symbol)
+        {
+            return $"\\u{(int)symbol:X4}";
+        }
+
+    }
+}
