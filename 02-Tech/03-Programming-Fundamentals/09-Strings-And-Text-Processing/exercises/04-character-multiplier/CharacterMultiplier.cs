edbff3de457ae77422435417c67c9e937b665ddf
Tectonik <marto7395@gmail.com>
2018-05-21 22:43:10 +0300

    Finished "Character multiplier"

---
+++ 02-Tech/03-Programming-Fundamentals/09-Strings-And-Text-Processing/exercises/04-character-multiplier/CharacterMultiplier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace _04_character_multiplier
+{
+    class CharacterMultiplier
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ');
+            var first = input[0];
+            var second = input[1];
+            var length = Math.Max(first.Length, second.Length);
+            BigInteger sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                var firstNumber = i < first.Length ? first[i] : 1;
+                var secondNumber = i < second.Length ? second[i] : 1;
+
+                sum += firstNumber * secondNumber;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
