43e0aca3190bb9b9fd4c34016146333fb0e0a8fb
Tectonik <marto7395@gmail.com>
2018-05-24 00:07:27 +0300

    Finished "Melrah shake"

---
+++ 02-Tech/03-Programming-Fundamentals/09-Strings-And-Text-Processing/exercises/09-melrah-shake/MelrahSnake.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace _09_melrah_shake
+{
+    class MelrahSnake
+    {
+        static void Main()
+        {
+            #if DEBUG
+            var testNumber = Console.ReadLine();
+            Console.SetIn(new StreamReader($"tests/test{testNumber}.txt"));
+            #endif
+
+            var characters = Console.ReadLine();
+            var pattern = Console.ReadLine();
+
+            while (true)
+            {
+                var firstMatchIndex = characters.IndexOf(pattern);
+                var lastMatchIndex = characters.LastIndexOf(pattern);
+
+                if (firstMatchIndex >= lastMatchIndex || pattern == string.Empty)
+                {
+                    Console.WriteLine("No shake.");
+                    break;
+                }
+
+                characters = characters.Remove(lastMatchIndex, pattern.Length);
+                characters = characters.Remove(firstMatchIndex, pattern.Length);
+                pattern = pattern.Remove(pattern.Length / 2, 1);
+
+                Console.WriteLine("Shaked it.");
+            }
+
+            Console.WriteLine(characters);
+        }
+    }
+}
