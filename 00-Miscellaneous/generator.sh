1c632aee4fcc0923ea60ebba445e38c262d6c93e
Tectonik <marto7395@gmail.com>
2018-05-25 10:35:41 +0300

    Modified generator to get project names from text file

--- 00-Miscellaneous/generator.sh
+++ 00-Miscellaneous/generator.sh
@@ -1,52 +1,54 @@
 #!/bin/bash
-
+currentDir=$(dirname "$0")
+cd $currentDir
 #===  FUNCTION  ================================================================
 #         NAME: pad
 #  DESCRIPTION: Pad $text on the $side with $char characters to length $length
 #   PARAMETERS: 1 - the text string to pad (no default)
 #               2 - how long the padded string is to be (default 80)
 #               3 - the character to pad with (default '-')
+#               4 - the side to pad on, L or R  or C for centre (default R)
+#      RETURNS: Nothing
 #===============================================================================
 pad () {
     local text=${1?Usage: pad text [length] [character] [L|R|C]}
     local length=${2:-80}
     local char=${3:--}
+    local side=${4:-R}
     local line l2
     #
     [ ${#text} -ge $length ] && { echo "$text"; return; }
     #
     char=${char:0:1}
+    side=${side^^}
     #
     printf -v line "%*s" $(($length - ${#text})) ' '
     line=${line// /$char}
     #
-    echo "${line}${text}"
+    if [[ $side == "R" ]]; then
+        echo "${text}${line}"
+        elif [[ $side == "L" ]]; then
+        echo "${line}${text}"
+        elif [[ $side == "C" ]]; then
+        l2=$((${#line}/2))
+        echo "${line:0:$l2}${text}${line:$l2}"
+    fi
 }
 
-projects=(
-    "Convert from Base-10 to Base-N"
-    "Convert from Base-N to Base-10"
-    "Unicode Characters"
-    "Character Multiplier"
-    "Magic Exchangeable Words"
-    "Sum Big Numbers"
-    "Multiply Big Number"
-    "Letters Change Numbers"
-    "Melrah Shake"
-)
+readarray -t projects < /home/tectonik/Desktop/SoftUni/02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/extra-exercises/projects.txt
 
-index=1
+current=1
 for i in "${projects[@]}"
 do
-    indexLength=$((2-${#index}))
-    nameLowercase=${i,,}
-    nameHyphenized=${nameLowercase//\ /-}
-    nameUnpadded=$index-$nameHyphenized
-    padding=$((indexLength+${#nameUnpadded}))
-    namePadded=$(pad $nameUnpadded $padding '0')
+    project=${i,,} #Lowercase
+    project=${project// /-} #Replace spaces with dashes
+    project=${project//[,?*]/} #Remove punctuation and stars
     #
-    echo $namePadded
-    ((index++))
-    # dotnet new console -o ${namePadded}
-    # echo ${padding} - ${#nameUnpadded} - ${indexLength} - ${#i}
+    index=$(pad $current 2 0 L)
+    #
+    echo $index-$project
+    # dotnet new console -o $index-$project
+    ((current++))
 done
+# echo $BASH_SOURCE
+# .,/#\!$%^&*;:-_\
