e7f2d8259e1b41b33b6c4489aa2c76f00240ec5a
Tectonik <marto7395@gmail.com>
2018-05-25 23:28:25 +0300

    Finished "Extract sentences by keyword"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/exercises/02-extract-sentences-by-keyword/ExtractSentencesByKeyword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02_extract_sentences_by_keyword
+{
+    class ExtractSentencesByKeyword
+    {
+        static void Main()
+        {
+            #if DEBUG
+            Console.SetIn(new StreamReader("test.txt"));
+            #endif
+
+            var keyword = Console.ReadLine();
+            var sentences = Console.ReadLine().Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var SentencesContainingKeyword = sentences.Where(sentence => Regex.IsMatch(sentence, $"\\b{keyword}\\b"));
+            foreach (var sentence in SentencesContainingKeyword)
+            {
+                Console.WriteLine(sentence);
+            }
+        }
+    }
+}
