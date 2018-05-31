18b5ac5b56838b6e44a61422b69164883805346c
Tectonik <marto7395@gmail.com>
2018-05-31 16:57:31 +0300

    Finished "Camera view"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/exercises/03-camera-view/CameraView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_camera_view
+{
+    class CameraView
+    {
+        static void Main()
+        {
+            var arguments = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var elements = Console.ReadLine().Split("|<");
+
+            var result = new List<string>();
+            foreach (var element in elements.Skip(1))
+            {
+                string view = string.Join("", element.Skip(arguments[0]).Take(arguments[1]));
+                result.Add(view);
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
+/*
+
+9 7
+GreatBigSea|<uglyStuffHawaii|<boriiiingKilauea
+
+0 5
+|>invalid|<beach|noMoreCameras
+
+ */
