17a5ff2f10bd781e9df05af87fb846cb453740fa
Tectonik <marto7395@gmail.com>
2018-06-01 21:34:57 +0300

    Working on "Key replacer"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/exercises/05-key-replacer/KeyReplacer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05_key_replacer
+{
+    class KeyReplacer
+    {
+        static void Main()
+        {
+            var keys = Regex.Split(Console.ReadLine(), @"[|</].*[|</]");
+            var pattern = new Regex($"{keys.First()}(.*?){keys.Last()}");
+            var text = Console.ReadLine();
+
+            var matches = pattern.Matches(text).Select(m => m.Groups[1].Value);
+            var message = string.Concat(matches);
+            if (message.Length > 0)
+            {
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("Empty result");
+            }
+        }
+    }
+}
+/*
+
+start<213asfaas|end
+saaastarthelloendsdarstartFromTheOtherenddvsefdsfstartSideend
+
+A|safafasfsadf|Csddsd|B
+AB12345BABAkjahdkjdhdghgdjhgB
+
+A|safafasfsadf|B
+NoMEssageABhereYeyAB
+
+ */
