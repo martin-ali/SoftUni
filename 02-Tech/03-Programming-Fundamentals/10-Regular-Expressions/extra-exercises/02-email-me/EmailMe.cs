7ab7ad479ada3c255b876b39035b0b205dbb7328
Tectonik <marto7395@gmail.com>
2018-06-01 21:43:22 +0300

    Finished "Email me"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/extra-exercises/02-email-me/EmailMe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_email_me
+{
+    class EmailMe
+    {
+        static void Main()
+        {
+            var email = Console.ReadLine();
+            var emailParts = email.Split('@');
+            var sumBeforeAt = emailParts[0]
+                                .Select(c => (int)c)
+                                .Aggregate((sum, c) => (sum + c));
+            var sumAfterAt = emailParts[1]
+                                .Select(c => (int)c)
+                                .Aggregate((sum, c) => (sum + c));
+
+            var result = sumBeforeAt - sumAfterAt;
+            if (result >= 0)
+            {
+                Console.WriteLine("Call her!");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one.");
+            }
+        }
+    }
+}
