b72a1ccdfa2b4571831d420b47d8938421ae4217
Tectonik <marto7395@gmail.com>
2018-06-01 21:34:21 +0300

    Finished "Censorship"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/extra-exercises/01-censorship/Censorship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_censorship
+{
+    class Censorship
+    {
+        static void Main()
+        {
+            var censor = Console.ReadLine();
+            var text = Console.ReadLine();
+
+            var censoredText = text.Replace(censor, new string('*', censor.Length));
+            Console.WriteLine(censoredText);
+        }
+    }
+}
