59ba4ef8f7e76a0661b1a58417fe32f26e56b7ff
Tectonik <marto7395@gmail.com>
2018-06-03 18:36:14 +0300

    Finished "Hideout"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/extra-exercises/07-hideout/Hideout.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _07_hideout
+{
+    class Hideout
+    {
+        static void Main()
+        {
+            var map = Console.ReadLine();
+            while (true)
+            {
+                var clue = Console.ReadLine().Split();
+
+                var element = Regex.Escape(clue[0]);
+                var size = int.Parse(clue[1]);
+
+                var pattern = new Regex($@"{element}{{{size},}}");
+                var match = pattern.Match(map);
+                if (match.Success)
+                {
+                    Console.WriteLine($"Hideout found at index {match.Index} and it is with size {match.Length}!");
+                    break;
+                }
+            }
+        }
+    }
+}
+/*
+
+asd@@asdasd@@@@@@@asdasd asdsad
+@ 5
+
+asd@@asd***asdasdsad123%4521Asdsad************ASssda
+&amp; 3
+* 20
+* 10
+* 2
+
+ */
