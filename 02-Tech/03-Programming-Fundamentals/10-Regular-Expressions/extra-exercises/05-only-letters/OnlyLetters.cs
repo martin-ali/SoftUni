2e2002aa14155a6786ed26c80c8a4906c8df0b3e
Tectonik <marto7395@gmail.com>
2018-06-03 14:04:32 +0300

    Finished "Only letters"

---
+++ 02-Tech/03-Programming-Fundamentals/10-Regular-Expressions/extra-exercises/05-only-letters/OnlyLetters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05_only_letters
+{
+    class OnlyLetters
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var replacedString = Regex.Replace(input, @"(\d+)([A-Za-z])",
+                                        x => string.Format("{0}{0}", x.Groups[2].Value));
+            Console.WriteLine(replacedString);
+        }
+    }
+}
+/*
+
+ChangeThis12andThis56k
+
+1Beware72ForThe4End88888
+
+ */
