a3e19231c5dc994d95570c3ee991da9f7e96b329
Tectonik <marto7395@gmail.com>
2018-06-13 12:14:33 +0300

    Finished "Files and exceptions" exercises

---
+++ 02-Tech/03-Programming-Fundamentals/11-Files-And-Exceptions/exercises/02-index-of-letters/IndexOfLetters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace _02_index_of_letters
+{
+    class IndexOfLetters
+    {
+        static void Main()
+        {
+            var letters = File.ReadAllText("input.txt");
+            for (int i = 0; i < letters.Length; i++)
+            {
+                string letterAndIndex = $"{letters[i]} -> {letters[i] - 97}";
+                File.AppendAllText("output.txt", letterAndIndex + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ 02-Tech/03-Programming-Fundamentals/11-Files-And-Exceptions/exercises/05-fix-emails/FixEmails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _05_fix_emails
+{
+    class FixEmails
+    {
+        static void Main()
+        {
+            var lines = File.ReadAllLines("input.txt").ToList();
+            var sanitizedLines = new List<string>();
+
+            for (int i = 0; lines[i] != "stop"; i += 2)
+            {
+                var name = lines[i];
+                var email = lines[i + 1];
+
+                var emailIsValid = email.EndsWith("uk") == false
+                                && email.EndsWith("us") == false;
+                if (emailIsValid)
+                {
+                    sanitizedLines.Add($"{name} -> {email}");
+                }
+            }
+
+            File.WriteAllLines("output.txt", sanitizedLines);
+        }
+    }
+}
