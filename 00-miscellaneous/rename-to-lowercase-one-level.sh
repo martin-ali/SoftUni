9605a39f1e11a10c43623a5cf425a27f9dbfcb5f
Tectonik <marto7395@gmail.com>
2018-06-23 18:32:19 +0300

    Added renamer script

---
+++ 00-miscellaneous/rename-to-lowercase-one-level.sh
@@ -0,0 +1,7 @@
+cd /home/tectonik/Desktop/SoftUni/02-tech/03-programming-fundamentals/12-exam-preparation/
+for fd in */; do
+  #get lower case version
+  fd_lower=$(printf %s "$fd" | tr A-Z a-z)
+  #if it wasn't already lowercase, move it.
+  [ "$fd" != "$fd_lower" ] && mv "$fd" "$fd_lower"
+done
