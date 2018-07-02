1a05382009a6a30a5ce964256fe1084e242898d8
Tectonik <marto7395@gmail.com>
2018-07-02 23:37:36 +0300

    Removed unnecessary alternative solution from "Soft Uni Reception". Re-named variable

--- 02-tech/01-programming-fundamentals/13-exam/01-softuni-reception/SoftuniReception.cs
+++ 02-tech/01-programming-fundamentals/13-exam/01-softuni-reception/SoftuniReception.cs
@@ -11,22 +11,17 @@ namespace _01_softuni_reception
             var thirdEmployee = int.Parse(Console.ReadLine());
             var studentCount = double.Parse(Console.ReadLine());
 
-            // var totalProcessingAbility = firstEmployee + secondEmployee + thirdEmployee;
-            // var workTime = Math.Ceiling(studentCount / totalProcessingAbility);
-            // var breakTime = Math.Floor(workTime / 4);
-            // var timeNeeded = workTime + breakTime;
-
-            var timeNeeded = 0;
+            var hoursNeeded = 0;
             while (studentCount > 0)
             {
-                timeNeeded++;
-                if (timeNeeded % 4 != 0)
+                hoursNeeded++;
+                if (hoursNeeded % 4 != 0)
                 {
-                    studentCount -= firstEmployee + secondEmployee + thirdEmployee;
+                    studentCount -= (firstEmployee + secondEmployee + thirdEmployee);
                 }
             }
 
-            Console.WriteLine($"Time needed: {timeNeeded}h.");
+            Console.WriteLine($"Time needed: {hoursNeeded}h.");
         }
     }
 }
