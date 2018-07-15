fb207279061964a1e605cb9f425fe47af1c07e89
Tectonik <marto7395@gmail.com>
2018-07-16 00:14:13 +0300

    Moved old "ExpressJS preview" Calculator to "calculator-old" folder

---
+++ 02-tech/03-software-technologies/04-javascript/02-expressjs-overview/calculator-old/models/calculator.js
@@ -0,0 +1,70 @@
+function Calculator(leftOperand, operator, rightOperand)
+{
+    'use strict';
+    this.leftOperand = Number(leftOperand);
+    this.operator = operator;
+    this.rightOperand = Number(rightOperand);
+
+    this.calculateResult = () =>
+    {
+        let result = 0;
+        switch (this.operator)
+        {
+            case '+':
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case '-':
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case '*':
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case '/':
+                result = this.leftOperand / this.rightOperand;
+                break;
+            case '^':
+                result = this.leftOperand ** this.rightOperand;
+                break;
+        }
+
+        return result;
+    }
+}
+
+module.exports = Calculator;
+
+// class Calculator
+// {
+//     'use strict';
+//     constructor(leftOperand, operator, rightOperand)
+//     {
+//         this.leftOperand = leftOperand;
+//         this.operator = operator;
+//         this.rightOperand = rightOperand;
+//     }
+
+//     calculateResult()
+//     {
+//         let result = 0;
+
+//         switch (this.operator)
+//         {
+//             case '+':
+//                 result = this.leftOperand + this.rightOperand;
+//                 break;
+//             case '-':
+//                 result = this.leftOperand - this.rightOperand;
+//                 break;
+//             case '*':
+//                 result = this.leftOperand * this.rightOperand;
+//                 break;
+//             case '/':
+//                 result = this.leftOperand / this.rightOperand;
+//                 break;
+//         }
+
+//         return result;
+//     }
+// }
+
+// export default Calculator;
