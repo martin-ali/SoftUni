8a649d01cb14960f17cd53a2fdb5b1fa6662fb3e
Tectonik <marto7395@gmail.com>
2018-07-31 17:19:05 +0300

    Played around with async-await-demo.js

--- 00-miscellaneous/async-await-demo.js
+++ 00-miscellaneous/async-await-demo.js
@@ -1,26 +1,22 @@
-function logDone(value)
+function sleep(milliseconds)
 {
-    return new Promise((resolve, reject) =>
-    {
-        setTimeout(resolve, 1000);
-        console.log(`Did ${value}`)
-    });
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
 // Async functions are executed in a new thread
-async function executeTasks()
+async function log(task)
 {
     // Await pauses code execution until it's done
     // So it sort of becomes single-threaded internally?
-    await logDone(1);
-    await logDone(2);
-    await logDone(3);
+    await sleep(1000);
+    console.log(`Task ${task}`);
 }
 
 (async function ()
 {
     // No await means the code continues execution after parsing the line and calling the function
-    executeTasks();
-    executeTasks();
-    executeTasks();
-})();
+    await log(1);
+    await log(2);
+    log(3);
+    log(4);
+}());
