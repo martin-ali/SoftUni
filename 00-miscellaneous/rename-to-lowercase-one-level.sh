7d9d23bcaca7f729dccb86c4c12db2bab0f9ef29
Tectonik <marto7395@gmail.com>
2018-07-31 17:18:04 +0300

    Fixed bad spelling in rename-to-lowercase-one-level.sh

--- 00-miscellaneous/rename-to-lowercase-one-level.sh
+++ 00-miscellaneous/rename-to-lowercase-one-level.sh
@@ -1,6 +1,6 @@
 cd /home/tectonik/Desktop/SoftUni/02-tech/03-programming-fundamentals/12-exam-preparation/
 for fd in */; do
-  #get lower case version
+  #get lowercase version
   fd_lower=$(printf %s "$fd" | tr A-Z a-z)
   #if it wasn't already lowercase, move it.
   [ "$fd" != "$fd_lower" ] && mv "$fd" "$fd_lower"
