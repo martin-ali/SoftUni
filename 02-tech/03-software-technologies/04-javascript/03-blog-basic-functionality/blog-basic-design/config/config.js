cbd8fdfb1c0f22e2bd40f4c9f2e865a976e58c4d
Tectonik <marto7395@gmail.com>
2018-08-07 16:24:00 +0300

    [Java Tech Web, Syntax] Modified project settings

--- 02-tech/03-software-technologies/04-javascript/03-blog-basic-functionality/blog-basic-design/config/config.js
+++ 02-tech/03-software-technologies/04-javascript/03-blog-basic-functionality/blog-basic-design/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
         {
             "username": "root",
             "password": null,
-            "database": "blogjs",
+            "database": "blogjs2",
             "host": "127.0.0.1",
             "dialect": "mysql"
         }
