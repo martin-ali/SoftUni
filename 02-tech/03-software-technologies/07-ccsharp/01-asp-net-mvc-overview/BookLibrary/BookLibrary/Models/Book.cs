539f963577f808e739178dc92c85300dc36dc1f9
Tectonik <marto7395@gmail.com>
2018-08-08 16:18:13 +0300

    [C# Tech Web] Working on Book Library

---
+++ 02-tech/03-software-technologies/07-ccsharp/01-asp-net-mvc-overview/BookLibrary/BookLibrary/Models/Book.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookLibrary.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        // string?
+        public int AuthorId { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
