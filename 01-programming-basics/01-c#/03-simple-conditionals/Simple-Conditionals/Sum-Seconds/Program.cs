f2cedb8802b4b6c25dbb2f4c0c06d969e0254e14
Tectonik <marto7395@gmail.com>
2018-08-13 08:11:40 +0300

    [Programming Basics] Re-named csharp folder to c#

---
+++ 01-programming-basics/01-c#/03-simple-conditionals/Simple-Conditionals/Sum-Seconds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sum_Seconds
+{
+    class Program
+    {
+        static void Main()
+        {
+            //var firstRunner = 14d;
+            //var secondRunner = 12d;
+            //var thirdRunner = 10d;
+
+            var firstRunner = double.Parse(Console.ReadLine());
+            var secondRunner = double.Parse(Console.ReadLine());
+            var thirdRunner = double.Parse(Console.ReadLine());
+
+            double totalNumberOfSeconds = firstRunner + secondRunner + thirdRunner;
+
+            double numberOfMinutes = Math.Truncate(totalNumberOfSeconds / 60);
+            double leftOverSeconds = totalNumberOfSeconds - (numberOfMinutes * 60);
+
+            Console.WriteLine($"{numberOfMinutes}:{leftOverSeconds.ToString().PadLeft(2, '0')}");           
+        }
+    }
+}
