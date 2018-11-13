fe382a70b3a3d028c3b1ac521e61cbf94a620461
Tectonik <marto7395@gmail.com>
2018-11-13 14:34:21 +0200

    [C# Advanced] Re-organised old "Sets and dictionaries" exercises

---
+++ 03-c#-fundamentals/01-c#-advanced/x02-sets-and-dictionaries/01-unique-usernames/UniqueUsernames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_unique_usernames
+{
+    public class UniqueUsernames
+    {
+        public static void Main()
+        {
+            var usernameCount = int.Parse(Console.ReadLine());
+
+            var uniqueUsernames = new HashSet<string>();
+            for (int i = 0; i < usernameCount; i++)
+            {
+                var username = Console.ReadLine();
+                uniqueUsernames.Add(username);
+            }
+
+            foreach (var username in uniqueUsernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
---
+++ 03-c#-fundamentals/01-c#-advanced/x02-sets-and-dictionaries/03-periodic-table/PeriodicTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_periodic_table
+{
+    class PeriodicTable
+    {
+        static void Main()
+        {
+            var compoundCount = int.Parse(Console.ReadLine());
+
+            var uniqueChemicalElements = new SortedSet<string>();
+            for (int i = 0; i < compoundCount; i++)
+            {
+                var compoundChemicalElements = Console.ReadLine().Split();
+                foreach (var chemicalElement in compoundChemicalElements)
+                {
+                    uniqueChemicalElements.Add(chemicalElement);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', uniqueChemicalElements));
+        }
+    }
+}
